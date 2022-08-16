1174b4c33cf03a339028f46fc1e7ec7f03f0e7c3
ryanha <ryanha@handong.ac.kr>
2022-08-16 15:15:22 +0900

    Update[BOJ - Q.2178]

--- BFS/BOJ_2178.java
+++ BFS/BOJ_2178.java
@@ -1,86 +1,69 @@
 // BOJ - 2178
 // Problem Sheet - https://www.acmicpc.net/problem/2178
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
+// 미로 상에서 좌표를 나타내는 클래스
 class Location {
-    private int x; // the 'x' coordinates
-    private int y; // the 'y' coordinates
+    private final int x;
+    private final int y;
 
-    // the constructor
     public Location(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    // getter
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
+    public int getX() { return this.x; }
+    public int getY() { return this.y; }
 }
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner key = new Scanner(System.in);
-        int n = key.nextInt(); // row
-        int m = key.nextInt(); // column
-        int[][] visit = new int[n+1][m+1]; // (0 - 방문하지 않음, 1 - 방문함, -1 : 지나갈 수 없는 영역)
-        String tmp;
-        key.nextLine();
-        for(int i=0 ; i<n ; i++) {
-            tmp = key.nextLine();
-            for(int j=0 ; j<m ; j++) {
-                int x = Integer.parseInt(tmp.charAt(j)+"");
-                if(x == 0) visit[i+1][j+1] = -1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 세로 길이
+        int m = Integer.parseInt(st.nextToken()); // 가로 길이
+        int[][] board = new int[n+1][m+1]; // 미로
+        int[][] visit = new int[n+1][m+1]; // 방문여부 저장 (0: 지날 수 없음, 1: 지날 수 있고 안지남, 2: 지날 수 있고 지남)
+        for(int i=1 ; i<=n ; i++) {
+            String tmp = bf.readLine();
+            for(int j=1 ; j<=m ; j++) {
+                board[i][j] = Integer.parseInt(tmp.charAt(j-1)+"");
+                visit[i][j] = Integer.parseInt(tmp.charAt(j-1)+"");
             }
         }
-        
-        Queue<Location> q = new LinkedList<>(); // BFS 알고리즘 동작을 위한 Queue
+        Queue<Location> queue = new LinkedList<>(); // bfs를 위한 queue
+        int[] xi = {0, 0, -1, 1}; // x increment
+        int[] yi = {-1, 1, 0, 0}; // y increment
 
-        // BFS 알고리즘의 동작에 대해서 기술하는 부분
-        visit[1][1] = 1;
-        q.add(new Location(1, 1));
-
-        while(!q.isEmpty()) {
-            int x = q.peek().getX();
-            int y = q.peek().getY();
-
-            // 원하는 목적지에 도달했는 가?
-            if(x == n && y == m) break;
-
-            // 좌로 이동이 가능한 가?
-            if(x>1 && visit[y][x-1] == 0) {
-                visit[y][x-1] = visit[y][x] + 1;
-                q.add(new Location(x-1, y));
+        // bfs를 통해 미로에 대한 솔루션을 찾음 - (1, 1에서 시작)
+        queue.add(new Location(1, 1));
+        visit[1][1] = 2;
+        while(!queue.isEmpty()) {
+            int curX = queue.peek().getX(); // 현재 위치의 x좌표
+            int curY = queue.peek().getY(); // 현재 위치의 y좌표
+            for(int i=0 ; i<4 ; i++) {
+                int tx = curX + xi[i];
+                int ty = curY + yi[i];
+                if(validation(tx, ty, m, n) && visit[ty][tx] == 1) {
+                    board[ty][tx] = board[curY][curX] + 1;
+                    queue.add(new Location(tx, ty));
+                    visit[ty][tx] = 2;
+                }
             }
-            // 우로 이동이 가능한 가?
-            if(x<m && visit[y][x+1] == 0) {
-                visit[y][x+1] = visit[y][x] + 1;
-                q.add(new Location(x+1, y));
-            }
-            // 위로 이동이 가능한 가?
-            if(y>1 && visit[y-1][x] == 0) {
-                visit[y-1][x] = visit[y][x] + 1;
-                q.add(new Location(x, y-1));
-            }
-            // 아래로 이동이 가능한 가?
-            if(y<n && visit[y+1][x] == 0) {
-                visit[y+1][x] = visit[y][x] + 1;
-                q.add(new Location(x, y+1));
-            }
-
-            q.poll();
+            queue.poll();
         }
 
-        System.out.println(visit[n][m]);
+        System.out.println(board[n][m]);
 
-        key.close();
+        bf.close();
         System.exit(0);
+    }
+
+    public static boolean validation(int x, int y, int xLimit, int yLimit) {
+        if(x<1 || y<1 || x>xLimit || y>yLimit)
+            return false;
+        return true;
     }
 }
