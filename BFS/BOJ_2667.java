254dc55d415fe42a28a998c9a680d9d9784b0c37
ryanha <ryanha@handong.ac.kr>
2022-08-18 17:00:13 +0900

    Update[BOJ - Q.2667]

--- BFS/BOJ_2667.java
+++ BFS/BOJ_2667.java
@@ -1,90 +1,82 @@
 // BOJ - 2667
 // Problem Sheet - https://www.acmicpc.net/problem/2667
 
-import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+import java.io.*;
 
 class Location {
-	private int x; // x-coordinate
-	private int y; // y-coordinate
+    private final int x;
+    private final int y;
 
-	public Location(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	// getter
-	public int getX() {
-		return this.x;
-	}
-
-	public int getY() {
-		return this.y;
-	}
+    public int getX() { return this.x; }
+    public int getY() { return this.y; }
 }
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner key = new Scanner(System.in);
-		int size = key.nextInt(); // map size
-		int[][] map = new int[size][size];
-		String tmp;
-		key.nextLine();
-		for(int i=0 ; i<size ; i++) {
-			tmp = key.nextLine();
-			for(int j=0 ; j<tmp.length() ; j++)
-				if(tmp.charAt(j) == '1')
-					map[i][j] = 1;
-		}
-		int[] xi = {0, 0, -1, 1}; // x-increment
-		int[] yi = {-1, 1, 0, 0}; // y-increment
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine()); // 지도의 크기
+        int[][] map = new int[n][n]; // 지도
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(); // 단지 수 저장
 
-		// search by using bfs algorithm
-		Queue<Location> q = new LinkedList<>(); // queue for bfs
-		int tmpCount = 0;
-		ArrayList<Integer> counts = new ArrayList<>(); // store the town count
-		for(int i=0 ; i<size ; i++) {
-			for(int j=0 ; j<size ; j++) {
-				if(map[i][j] == 1) {
-					map[i][j] = 2; // visit
-					tmpCount++;
-					q.add(new Location(j, i));
-					while(!q.isEmpty()) {
-						int tx = q.peek().getX();
-						int ty = q.peek().getY();
-						for(int k=0 ; k<4 ; k++) {
-							int curX = tx + xi[k];
-							int curY = ty + yi[k];
-							if(validation(curX, curY, size) && map[curY][curX] == 1) {
-								map[curY][curX] = 2;
-								tmpCount++;
-								q.add(new Location(curX, curY));
-							}
-						}
-						q.poll();
-					}
-					counts.add(tmpCount);
-					tmpCount = 0; // initialize for reusing
-				}
-			}
-		}
+        // 입력 내용으로부터 지도를 구성함
+        for(int i=0 ; i<n ; i++) {
+            String tmp = bf.readLine();
+            for(int j=0 ; j<n ; j++)
+                map[i][j] = Integer.parseInt(tmp.charAt(j) + "");
+        }
 
-		Collections.sort(counts); // sort the counts	
-		System.out.println(counts.size()); // the number of complex
-		for(int count : counts)
-			System.out.println(count);
+        // 지도를 분석함
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                if(map[i][j] == 1)
+                    pQueue.add(bfs(map, j, i));
+            }
+        }
 
-		key.close();
-		System.exit(0);
-	}
+        // 수집된 정보를 출력
+        System.out.println(pQueue.size());
+        while(!pQueue.isEmpty())
+            System.out.println(pQueue.poll());
 
-	public static boolean validation(int x, int y, int size) {
-		if(x<0 || x>=size || y<0 || y>=size)
-			return false;
-		else
-			return true;
-	}
+        bf.close();
+        System.exit(0);
+    }
+
+    public static int bfs(int[][] map, int startX, int startY) {
+        Queue<Location> queue = new LinkedList<>(); // bfs를 위해 사용할 queue
+        int[] xi = {-1, 1, 0, 0};
+        int[] yi = {0, 0, -1, 1};
+        int townSize = 0; // 단지 수
+
+        map[startY][startX] = 2;
+        queue.add(new Location(startX, startY));
+        townSize++;
+        while(!queue.isEmpty()) {
+            int curX = queue.peek().getX();
+            int curY = queue.peek().getY();
+            for(int i=0 ; i<4 ; i++) {
+                int tx = curX + xi[i];
+                int ty = curY + yi[i];
+                if(validation(tx, ty, map.length) && map[ty][tx] == 1) {
+                    map[ty][tx] = 2;
+                    queue.add(new Location(tx, ty));
+                    townSize++;
+                }
+            }
+            queue.poll();
+        }
+        return townSize;
+    }
+
+    public static boolean validation(int x, int y, int n) {
+        if(x<0 || y<0 || x>n-1 || y>n-1)
+            return false;
+        return true;
+    }
 }
