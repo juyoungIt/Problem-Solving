91bce4818248824645aca844952a1b231b649a42
ryanha <ryanha@handong.ac.kr>
2023-04-29 19:42:07 +0900

    Solution[BOJ - Q.2743]

---
+++ String/BOJ_2743.java
@@ -0,0 +1,16 @@
+// BOJ - 2743
+// Problem Sheet - https://www.acmicpc.net/problem/2743
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(bf.readLine().length());
+
+        bf.close();
+        System.exit(0);
+    }
+}
