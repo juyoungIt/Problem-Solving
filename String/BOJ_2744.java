01242dda8069d9e8e3bc3b28a00faa834d042a91
ryanha <ryanha@handong.ac.kr>
2023-06-10 10:10:38 +0900

    Refactor[BOJ - Q.2744]

--- String/BOJ_2744.java
+++ String/BOJ_2744.java
@@ -1,22 +1,22 @@
 // BOJ - 2744
 // Problem Sheet - https://www.acmicpc.net/problem/2744
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String input = bf.readLine();
 
-        for(char letter : input.toCharArray()) {
-            if((int)letter >= 65 && (int)letter <= 90) {
+        String input = bf.readLine();
+        for(int i=0 ; i<input.length() ; i++) {
+            char letter = input.charAt(i);
+            if(Character.isUpperCase(letter)) {
                 sb.append(Character.toLowerCase(letter));
-            } else if((int)letter >= 97 && (int)letter <= 122) {
+            } else if(Character.isLowerCase(letter)) {
                 sb.append(Character.toUpperCase(letter));
             } else {
-                throw new IllegalArgumentException("Invalid Arguments");
+                throw new IllegalArgumentException();
             }
         }
 
