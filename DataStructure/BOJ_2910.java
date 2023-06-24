9b8eedf9ca47099ca2298340a4d127acf1892b90
ryanha <ryanha@handong.ac.kr>
2023-06-24 16:55:41 +0900

    Refactor[BOJ - Q.2910] : 정렬코드 일부 개선

--- DataStructure/BOJ_2910.java
+++ DataStructure/BOJ_2910.java
@@ -1,56 +1,55 @@
-// BOJ - 2910
-// Problem Sheet - https://www.acmicpc.net/problem/2910
+// BOJ - 18870
+// Problem Sheet - https://www.acmicpc.net/problem/18870
 
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
-    static class NumberInfo implements Comparable<NumberInfo> {
-        private int number;
-        private int count;
+    static class Number implements Comparable<Number> {
+        private final String value;
+        private final int useCount;
 
-        public NumberInfo(int number, int count) {
-            this.number = number;
-            this.count = count;
+        public Number(String value, int useCount) {
+            this.value = value;
+            this.useCount = useCount;
         }
 
-        public int getNumber() { return this.number; }
-        public int getCount() { return this.count; }
+        public String getValue() { return this.value; }
+        public int getUseCount() { return this.useCount; }
 
         @Override
-        public int compareTo(NumberInfo numberInfo) {
-            return Integer.compare(this.count, numberInfo.getCount()) * -1;
+        public int compareTo(Number number) {
+            return number.getUseCount() - this.useCount;
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        Map<Integer, Integer> countMapper = new LinkedHashMap<>(); // 각 수의 사용된 횟수를 유지
-        List<NumberInfo> numbers = new ArrayList<>();
-        StringBuilder sb = new StringBuilder(); // 출력할 결과를 저장함
 
-        int n = Integer.parseInt(st.nextToken()); // 메시지의 길이
-        int c = Integer.parseInt(st.nextToken()); // 크기 제한 숫자
-
-        // 수열의 빈도수 정보를 종합함
+        int lenghtOfMessage  = Integer.parseInt(st.nextToken());
+        int maxValue = Integer.parseInt(st.nextToken());
+        Map<String, Integer> countMapper = new LinkedHashMap<>();
         st = new StringTokenizer(bf.readLine());
-        for(int i=0 ; i<n ; i++) {
-            int number = Integer.parseInt(st.nextToken());
-            if(!countMapper.containsKey(number)) countMapper.put(number, 1);
-            else countMapper.put(number, countMapper.get(number) + 1);
+        for(int i=0 ; i<lenghtOfMessage ; i++) {
+            String number = st.nextToken();
+            countMapper.put(number, countMapper.getOrDefault(number, 0) + 1);
         }
 
-        // 종합된 빈도 수를 바탕으로 정렬함
-        for(Map.Entry<Integer, Integer> number : countMapper.entrySet())
-            numbers.add(new NumberInfo(number.getKey(), number.getValue()));
-        Collections.sort(numbers); // 빈도수 기준으로 오름차순 정렬함
+        List<Number> numbers = new LinkedList<>();
+        for(Map.Entry<String, Integer> entry : countMapper.entrySet()) {
+            numbers.add(new Number(entry.getKey(), entry.getValue()));
+        }
+        Collections.sort(numbers);
 
-        // 종합된 정렬 기준을 출력
-        for(NumberInfo number : numbers)
-            for(int i=0 ; i<number.getCount() ; i++)
-                sb.append(number.getNumber()).append(' ');
+        for(Number number : numbers) {
+            for(int i=0 ; i<number.getUseCount() ; i++) {
+                sb.append(number.getValue()).append(" ");
+            }
+        }
+
         System.out.println(sb);
 
         bf.close();
