69df9b75f053312caadd7ce241a46a427ff9d5b1
ryanha <ryanha@handong.ac.kr>
2023-06-24 15:38:13 +0900

    Refactor[BOJ - Q.1302] : Map을 사용하지 않고 풀이

--- Sorting/BOJ_1302.java
+++ Sorting/BOJ_1302.java
@@ -5,49 +5,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static class Book {
-        private final String name;
-        private final int soldCount;
-
-        public Book(String name, int soldCount) {
-            this.name = name;
-            this.soldCount = soldCount;
-        }
-
-        public String getName() { return this.name; }
-        public int getSoldCount() { return this.soldCount; }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int numberOfSoledBooks = Integer.parseInt(bf.readLine());
-        Map<String, Integer> soldCountMapper = new HashMap<>();
-        for(int i=0 ; i<numberOfSoledBooks ; i++) {
-            String bookName = bf.readLine();
-            if(!soldCountMapper.containsKey(bookName)) {
-                soldCountMapper.put(bookName, 0);
+        int numberOfSoldBooks = Integer.parseInt(bf.readLine());
+        String[] books = new String[numberOfSoldBooks];
+        for(int i=0 ; i<numberOfSoldBooks ; i++) {
+            books[i] = bf.readLine();
+        }
+        Arrays.sort(books);
+
+        String maxTitle = books[0];
+        int maxCount = 1;
+        int currentCount = 1;
+        for(int i=1 ; i<books.length ; i++) {
+            if(!books[i].equals(books[i-1])) {
+                currentCount = 0;
             }
-            soldCountMapper.put(bookName, soldCountMapper.get(bookName) + 1);
+            currentCount++;
+            if(maxCount < currentCount) {
+                maxCount = currentCount;
+                maxTitle = books[i];
+            }
         }
 
-        Book[] books = new Book[soldCountMapper.size()];
-        int curSize = 0;
-        for(Map.Entry<String, Integer> entry : soldCountMapper.entrySet()) {
-            books[curSize++] = new Book(entry.getKey(), entry.getValue());
-        }
-
-        Arrays.sort(books, (o1, o2) -> {
-            if(o1.getSoldCount() == o2.getSoldCount()) {
-                return o2.getName().compareTo(o1.getName());
-            }
-            return o1.getSoldCount() - o2.getSoldCount();
-        });
-
-        sb.append(books[soldCountMapper.size()-1].getName()).append("\n");
-        System.out.println(sb);
+        System.out.println(maxTitle);
 
         bf.close();
         System.exit(0);
