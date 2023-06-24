ffd42d55591f7b84f20aba45e4d481abc0ed7813
ryanha <ryanha@handong.ac.kr>
2023-06-24 14:22:18 +0900

    Solution[BOJ - Q.7785]

---
+++ Sorting/BOJ_7785.java
@@ -0,0 +1,38 @@
+// BOJ - 7785
+// Problem Sheet - https://www.acmicpc.net/problem/7785
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int numberOfLogs = Integer.parseInt(bf.readLine());
+        Set<String> entered = new HashSet<>();
+        for(int i=0 ; i<numberOfLogs ; i++) {
+            st = new StringTokenizer(bf.readLine());
+            String name = st.nextToken();
+            if(st.nextToken().equals("enter")) {
+                entered.add(name);
+            } else {
+                entered.remove(name);
+            }
+        }
+
+        String[] members = new String[entered.size()];
+        entered.toArray(members);
+        Arrays.sort(members, (o1, o2) -> o2.compareTo(o1));
+
+        for(String member : members) {
+            sb.append(member).append("\n");
+        }
+
+        System.out.println(sb);
+
+        bf.close();
+        System.exit(0);
+    }
+}
