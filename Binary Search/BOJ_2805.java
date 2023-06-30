b181395e4ea6c4b50d4113934dbd935cc493176f
ryanha <htj3460@gmail.com>
2023-06-30 13:33:58 +0900

    Solution[BOJ - Q.2805]

---
+++ Binary Search/BOJ_2805.java
@@ -0,0 +1,44 @@
+// BOJ - 2805
+// Problem Sheet - https://www.acmicpc.net/problem/2805
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] trees = new int[N];
+        st = new StringTokenizer(bf.readLine());
+        for(int i=0 ; i<N ; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(trees);
+
+        int bottom = 0;
+        int top = trees[N-1];
+        int middle;
+        int maxHeight = 0;
+        while(bottom < top) {
+            middle = (bottom + top) / 2;
+            long sum = 0;
+            for(int tree : trees) {
+                sum += Math.max(tree - middle, 0);
+            }
+            if(sum < M) {
+                top = middle;
+            } else {
+                maxHeight = middle;
+                bottom = middle + 1;
+            }
+        }
+
+        System.out.println(maxHeight);
+
+        bf.close();
+        System.exit(0);
+    }
+}
