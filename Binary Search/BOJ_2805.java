79c8c23b740388fd6536b290dbd563d048ddc6e8
Juyoung Ha <htj3460@gmail.com>
2023-07-01 20:21:41 +0900

    Refactor[BOJ - Q.2805] : 불필요한 동작제거를 통한 성능개선

--- Binary Search/BOJ_2805.java
+++ Binary Search/BOJ_2805.java
@@ -8,37 +8,46 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+        int optimalHeight = -1;
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] trees = new int[N];
-        st = new StringTokenizer(bf.readLine());
-        for(int i=0 ; i<N ; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(trees);
 
-        int bottom = 0;
-        int top = trees[N-1];
-        int middle;
+        int minHeight = 0;
+        int[] heights = new int[N];
         int maxHeight = 0;
-        while(bottom < top) {
-            middle = (bottom + top) >> 1;
-            long sum = 0;
-            for(int tree : trees) {
-                sum += Math.max(tree - middle, 0);
-            }
-            if(sum < M) {
-                top = middle;
-            } else {
-                maxHeight = middle;
-                bottom = middle + 1;
+        st = new StringTokenizer(bf.readLine());
+        for(int i=0 ; i<heights.length ; i++) {
+            heights[i] = Integer.parseInt(st.nextToken());
+            if(heights[i] > maxHeight) {
+                maxHeight = heights[i];
             }
         }
 
-        System.out.println(maxHeight);
+        int cutterHeight;
+        long totalLength;
+        while(minHeight <= maxHeight) {
+            cutterHeight = (minHeight + maxHeight) / 2;
+            totalLength = getLength(heights, cutterHeight);
+            if(totalLength >= M) {
+                optimalHeight = cutterHeight;
+                minHeight = cutterHeight + 1;
+            } else {
+                maxHeight = cutterHeight - 1;
+            }
+        }
+
+        System.out.println(optimalHeight);
 
         bf.close();
         System.exit(0);
+    }
+
+    private static long getLength(int[] heights, int cutterHeight) {
+        long totalLength = 0;
+        for(int height : heights) {
+            totalLength += Math.max(height - cutterHeight, 0);
+        }
+        return totalLength;
     }
 }
