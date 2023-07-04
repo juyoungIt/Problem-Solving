82c51db5745b8ed707fa7b610bdc092aff274d13
Juyoung Ha <htj3460@gmail.com>
2023-07-04 21:26:03 +0900

    Solution[BOJ - Q.2230]

---
+++ Two Pointer/BOJ_2230.java
@@ -0,0 +1,37 @@
+// BOJ - 2230
+// Problem Sheet - https://www.acmicpc.net/problem/2230
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] sequence = new int[N];
+        for(int i=0 ; i<N ; i++) {
+            sequence[i] = Integer.parseInt(bf.readLine());
+        }
+        Arrays.sort(sequence);
+
+        int rightIndex = 1;
+        int minDiff = 2000000001;
+        for(int i=0 ; i<N-1 ; i++) {
+            int diff = Math.abs(sequence[i] - sequence[rightIndex]);
+            while(diff < M && rightIndex < N-1) {
+                diff = Math.abs(sequence[i] - sequence[++rightIndex]);
+            }
+            if(diff >= M) {
+                minDiff = Math.min(minDiff, diff);
+            }
+        }
+
+        System.out.println(minDiff);
+
+        bf.close();
+        System.exit(0);
+    }
+}
