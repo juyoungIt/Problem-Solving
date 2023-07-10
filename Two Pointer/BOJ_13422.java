404526ad7b8a9715e7edb4fc442ca8eb8e152c93
ryanha <htj3460@gmail.com>
2023-07-10 14:13:25 +0900

    Solution[BOJ - Q.13422]

---
+++ Two Pointer/BOJ_13422.java
@@ -0,0 +1,56 @@
+// BOJ - 13422
+// Problem Sheet - https://www.acmicpc.net/problem/13422
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(bf.readLine());
+        for(int i=0 ; i<T ; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(bf.readLine());
+            int[] homes = new int[N+1];
+            int[] accHomes = new int[N+1];
+            for(int j=1 ; j<=N ; j++) {
+                homes[j] = Integer.parseInt(st.nextToken());
+                accHomes[j] = accHomes[j-1] + homes[j];
+            }
+            if(N == M) {
+                sb.append(Math.min(1, getPossibleCaseCount(accHomes, N, M, K))).append("\n");
+                continue;
+            }
+            sb.append(getPossibleCaseCount(accHomes, N, M, K)).append("\n");
+        }
+
+        System.out.println(sb);
+
+        bf.close();
+        System.exit(0);
+    }
+
+    private static int getPossibleCaseCount(int[] accHomes, int N, int M, int K) {
+        int possibleCaseCount = 0;
+        int nextIndex = M;
+        for(int i=1 ; i<=N ; i++) {
+            if(i <= nextIndex) {
+                if(accHomes[nextIndex] - accHomes[i-1] < K) {
+                    possibleCaseCount++;
+                }
+            } else {
+                if(accHomes[N] - accHomes[i-1] + accHomes[nextIndex] < K) {
+                    possibleCaseCount++;
+                }
+            }
+            nextIndex = (++nextIndex % N == 0) ? N : nextIndex % N;
+        }
+        return possibleCaseCount;
+    }
+}
