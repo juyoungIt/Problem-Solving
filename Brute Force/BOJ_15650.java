802ae13983085de0631e2d7962d66edfad4f3a31
juyoungha <htj3460@gmail.com>
2023-08-31 23:11:30 +0900

    Solution[BOJ - Q.15650]

---
+++ Brute Force/BOJ_15650.java
@@ -0,0 +1,48 @@
+// BOJ - 15650
+// Problem Sheet - https://www.acmicpc.net/problem/15650
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int N;
+    private static int M;
+    private static int[] selected;
+    private static boolean[] used;
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M];
+        used = new boolean[N+1];
+
+        generateCase(0);
+        System.out.println(sb);
+
+        bf.close();
+        System.exit(0);
+    }
+
+    private static void generateCase(int index) {
+        if(index >= M) {
+            Arrays.stream(selected).forEach(element -> sb.append(element).append(" "));
+            sb.append("\n");
+        } else {
+            for(int i=1 ; i<=N ; i++) {
+                if(index > 0 && selected[index-1] > i || used[i]) {
+                    continue;
+                }
+                selected[index] = i;
+                used[i] = true;
+                generateCase(index+1);
+                used[i] = false;
+            }
+        }
+    }
+}
