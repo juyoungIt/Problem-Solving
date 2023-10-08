2c6b47e7a52a97d7aa370d1bd12ad51d6a3c7e73
juyoungha <htj3460@gmail.com>
2023-10-08 11:28:47 +0900

    Solution[BOJ - Q.15655]

---
+++ Brute Force/BOJ_15655.java
@@ -0,0 +1,51 @@
+// BOJ - 15655
+// Problem Sheet - https://www.acmicpc.net/problem/15655
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int n;
+    private static int m;
+    private static int[] elements;
+    private static int[] selected;
+    private static StringBuilder result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        result = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        elements = new int[n];
+        selected = new int[m];
+
+        st = new StringTokenizer(bf.readLine());
+        for(int i=0 ; i<n ; i++) {
+            elements[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(elements);
+
+        generate(0, 0);
+        System.out.println(result);
+
+        bf.close();
+        System.exit(0);
+    }
+
+    private static void generate(int curLen, int lastIdx) {
+        if(curLen == m) {
+            for(int element : selected) {
+                result.append(element).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+        for(int i=lastIdx ; i<n ; i++) {
+            selected[curLen] = elements[i];
+            generate(curLen + 1, i + 1);
+        }
+    }
+}
