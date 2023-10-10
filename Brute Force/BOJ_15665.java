b9d517352e76328a0d3bda1840960787bb7e0cf6
juyoungha <htj3460@gmail.com>
2023-10-10 20:36:33 +0900

    Solution[BOJ - Q.15665]

---
+++ Brute Force/BOJ_15665.java
@@ -0,0 +1,58 @@
+// BOJ - 15665
+// Problem Sheet - https://www.acmicpc.net/problem/15665
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int m;
+    private static int[] elements;
+    private static int[] selected;
+    private static final StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        elements = new int[n];
+        selected = new int[m];
+
+        st = new StringTokenizer(bf.readLine());
+        for(int i=0 ; i<n ; i++) {
+            elements[i] = Integer.parseInt(st.nextToken());
+        }
+        elements = getUniqueArray();
+        Arrays.sort(elements);
+
+        generate(0);
+        System.out.println(result);
+
+        bf.close();
+        System.exit(0);
+    }
+
+    private static int[] getUniqueArray() {
+        Set<Integer> elementSet = new HashSet<>();
+        for(int element : elements) {
+            elementSet.add(element);
+        }
+        return elementSet.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static void generate(int accLen) {
+        if(accLen == m) {
+            for(int element : selected) {
+                result.append(element).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+        for(int element : elements) {
+            selected[accLen] = element;
+            generate(accLen + 1);
+        }
+    }
+}
