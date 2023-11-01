83f4d85bebd179f91a671a3ef58d138381dec737
Ryan.ha <htj3460@gmail.com>
2023-11-01 23:48:34 +0900

    Solution[BOJ - Q.28278] : LinkedList 기반으로 구현

--- DataStructure/BOJ_28278.java
+++ DataStructure/BOJ_28278.java
@@ -6,44 +6,104 @@ import java.io.*;
 
 public class Main {
 
-    static class MyStack {
+    static class MyNode {
 
-        private final int[] arr;
+        private final int value;
+        private MyNode next;
+        private final MyNode prev;
 
+        public MyNode(int value, MyNode prev) {
+            this.value = value;
+            this.prev = prev;
+            this.next = null;
+        }
+
+        public int getValue() { return this.value; }
+        public MyNode getPrev() { return this.prev; }
+        public MyNode getNext() { return this.next; }
+        public void setNext(MyNode next) { this.next = next; }
+
+    }
+
+    static class MyLinkedList {
+
+        private MyNode head;
+        private MyNode tail;
         private int size;
 
-        public MyStack() {
-            arr = new int[1_000_000];
-            this.size = 0;
+        public MyLinkedList() {
+            this.head = null;
+            this.tail = null;
         }
 
-        public void push(int x) {
-            arr[size++] = x;
+        public void add(int x) {
+            if(this.head == null) {
+                this.head = new MyNode(x, null);
+                this.tail = this.head;
+            } else {
+                this.tail.setNext(new MyNode(x, tail));
+                this.tail = this.tail.getNext();
+            }
+            this.size++;
         }
 
-        public int pop() {
-            if(size == 0) {
+        public int remove() {
+            if(this.size == 0) {
                 return -1;
             }
-            return arr[--size];
+            int removed = this.tail.getValue();
+            this.tail = this.tail.getPrev();
+            if(this.tail != null) {
+                this.tail.setNext(null);
+            } else {
+                this.head = null;
+            }
+            this.size--;
+            return removed;
+        }
+
+        public int getTail() {
+            return this.tail.getValue();
         }
 
         public int size() {
             return this.size;
         }
 
+    }
+
+    static class MyStack {
+
+        private final MyLinkedList storage;
+
+        public MyStack() {
+            this.storage = new MyLinkedList();
+        }
+
+        public void push(int x) {
+            this.storage.add(x);
+        }
+
+        public int pop() {
+            return this.storage.remove();
+        }
+
+        public int size() {
+            return this.storage.size;
+        }
+
         public int isEmpty() {
-            if(size == 0) {
+            if(this.storage.size() == 0) {
                 return 1;
             }
-            return 0;
+             return 0;
         }
 
         public int peek() {
-            if(size == 0) {
+            if(this.storage.size() == 0) {
                 return -1;
             }
-            return arr[size - 1];
+            return this.storage.getTail();
         }
 
     }
