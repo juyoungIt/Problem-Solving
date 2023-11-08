9c9e923309827cdebcfde9a59f59ddfdbd286b05
Ryan.ha <htj3460@gmail.com>
2023-11-08 20:56:42 +0900

    Solution[BOJ - Q.2251] : use bfs

---
+++ BFS/BOJ_2251.java
@@ -0,0 +1,79 @@
+// BOJ - 2251
+// Problem Sheet - https://www.acmicpc.net/problem/2251
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Bottle {
+
+        private final int bottleA;
+        private final int bottleB;
+        private final int bottleC;
+
+        public Bottle(int bottleA, int bottleB, int bottleC) {
+            this.bottleA = bottleA;
+            this.bottleB = bottleB;
+            this.bottleC = bottleC;
+        }
+
+        public int getBottleStatusById(int index) {
+            if(index == 0) return this.bottleA;
+            else if(index == 1) return this.bottleB;
+            else return this.bottleC;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int[] capas = new int[3];
+        capas[0] = Integer.parseInt(st.nextToken());
+        capas[1] = Integer.parseInt(st.nextToken());
+        capas[2] = Integer.parseInt(st.nextToken());
+        boolean[] isGenerated = new boolean[capas[2] + 1];
+
+        Set<String> set = new HashSet<>();
+        Queue<Bottle> queue = new LinkedList<>();
+        queue.add(new Bottle(0, 0, capas[2]));
+        isGenerated[capas[2]] = true;
+        while(!queue.isEmpty()) {
+            Bottle curStatus = queue.peek();
+            for(int src=0 ; src<3 ; src++) {
+                if(curStatus.getBottleStatusById(src) == 0) continue;
+                for(int dest=0 ; dest<3 ; dest++) {
+                    int[] bottles = new int[3];
+                    bottles[0] = curStatus.getBottleStatusById(0);
+                    bottles[1] = curStatus.getBottleStatusById(1);
+                    bottles[2] = curStatus.getBottleStatusById(2);
+                    if(dest == src || capas[dest] == bottles[dest]) continue;
+                    int quantity = Math.min(capas[dest] - bottles[dest], bottles[src]);
+                    bottles[src] -= quantity;
+                    bottles[dest] += quantity;
+                    if(!set.contains(Arrays.toString(bottles))) {
+                        queue.add(new Bottle(bottles[0], bottles[1], bottles[2]));
+                        set.add(Arrays.toString(bottles));
+                        if(bottles[0] == 0 && !isGenerated[bottles[2]]) {
+                            isGenerated[bottles[2]] = true;
+                        }
+                    }
+                }
+            }
+            queue.poll();
+        }
+
+        for(int i=0 ; i<isGenerated.length ; i++) {
+            if(isGenerated[i]) {
+                sb.append(i).append(" ");
+            }
+        }
+        System.out.println(sb);
+
+        br.close();
+        System.exit(0);
+    }
+}
