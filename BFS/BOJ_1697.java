fa4a718299b4e26de80bb07e9b86132cba856e14
juyoungha <htj3460@gmail.com>
2023-11-20 23:38:18 +0900

    Solution[BOJ - Q.1697] : 코드정리 및 가독성 개선

--- BFS/BOJ_1697.java
+++ BFS/BOJ_1697.java
@@ -4,56 +4,82 @@
 import java.util.*;
 import java.io.*;
 
-class Node {
-    private final int value; // Node가 저장하는 값
-    private final int depth; // 그래프가 탐색을 진행한 Depth
+public class Main {
 
-    public Node(int value, int depth) {
-        this.value = value;
-        this.depth = depth;
+    static class Node {
+        private final int location;
+        private final int time;
+
+        public Node(int location, int time) {
+            this.location = location;
+            this.time = time;
+        }
+
+        public int getLocation() { return this.location; }
+        public int getTime() { return this.time; }
     }
 
-    public int getValue() { return this.value; }
-    public int getDepth() { return this.depth; }
-}
-
-public class Main {
-    private static final int LOWER_LIMIT = 0; // 탐색 가능한 점 위치의 하한
-    private static final int UPPER_LIMIT = 100000; // 탐색 가능한 점 위치의 상한
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        int start = Integer.parseInt(st.nextToken()); // 탐색을 시작하는 시작점
-        int target = Integer.parseInt(st.nextToken()); // 도달, 찾고자 하는 지점
-        Queue<Node> queue = new LinkedList<>(); // bfs에 사용하는 queue
-        boolean[] visit = new boolean[UPPER_LIMIT+1]; // 해당 수에 대한 방문여부를 저장
-        int answer = 0; // 탐색에 소요된 최단시간
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        queue.add(new Node(start, 0));
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(searchFastestTime(n, k));
+
+        br.close();
+        System.exit(0);
+    }
+
+    private static int searchFastestTime(int src, int dest) {
+        Queue<Node> queue = new LinkedList<>();
+        boolean[] isVisit = new boolean[100_001];
+        int fastestTime = -1;
+
+        queue.add(new Node(src, 0));
+        isVisit[src] = true;
         while(!queue.isEmpty()) {
-            Node front = queue.peek();
-            if(front.getValue() == target) {
-                answer = front.getDepth();
+            Node cur = queue.peek();
+            int curLocation = cur.getLocation();
+            int curTime = cur.getTime();
+            if(curLocation == dest) {
+                fastestTime = curTime;
                 break;
             }
-            if(front.getValue()-1 >= LOWER_LIMIT && front.getValue()-1 <= UPPER_LIMIT && !visit[front.getValue()-1]) {
-                queue.add(new Node(front.getValue()-1, front.getDepth()+1));
-                visit[front.getValue()-1] = true;
+            int newLocation = moveLeft(curLocation);
+            if(isValid(newLocation) && !isVisit[newLocation]) {
+                queue.add(new Node(newLocation, curTime + 1));
+                isVisit[newLocation] = true;
             }
-            if(front.getValue()+1 >= LOWER_LIMIT && front.getValue()+1 <= UPPER_LIMIT && !visit[front.getValue()+1]) {
-                queue.add(new Node(front.getValue()+1, front.getDepth()+1));
-                visit[front.getValue()+1] = true;
+            newLocation = moveRight(curLocation);
+            if(isValid(newLocation) && !isVisit[newLocation]) {
+                queue.add(new Node(newLocation, curTime + 1));
+                isVisit[newLocation] = true;
             }
-            if(front.getValue()<<1 >= LOWER_LIMIT && front.getValue()<<1 <= UPPER_LIMIT && !visit[front.getValue()<<1]) {
-                queue.add(new Node(front.getValue()<<1, front.getDepth()+1));
-                visit[front.getValue()<<1] = true;
+            newLocation = teleport(curLocation);
+            if(isValid(newLocation) && !isVisit[newLocation]) {
+                queue.add(new Node(newLocation, curTime + 1));
+                isVisit[newLocation] = true;
             }
             queue.poll();
         }
+        return fastestTime;
+    }
 
-        System.out.println(answer);
+    private static boolean isValid(int location) {
+        return location >= 0 && location <= 100_000;
+    }
 
-        bf.close();
-        System.exit(0);
+    private static int moveLeft(int curLocation) {
+        return curLocation - 1;
+    }
+
+    private static int moveRight(int curLocation) {
+        return curLocation + 1;
+    }
+
+    private static int teleport(int curLocation) {
+        return curLocation * 2;
     }
 }
