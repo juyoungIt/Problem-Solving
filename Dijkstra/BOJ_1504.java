85bb37913830b8c65f9200f31328d4048a8d8410
juyoungha <htj3460@gmail.com>
2023-12-17 21:41:46 +0900

    Solution[BOJ - Q.1504]

---
+++ Dijkstra/BOJ_1504.java
@@ -0,0 +1,92 @@
+// BOJ - 1504
+// Problem Sheet - https://www.acmicpc.net/problem/1504
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Vertex implements Comparable<Vertex> {
+        private final int target;
+        private final int dist;
+
+        public Vertex(int target, int dist) {
+            this.target = target;
+            this.dist = dist;
+        }
+
+        public int getTarget() { return this.target; }
+        public int getDist() { return this.dist; }
+
+        @Override
+        public int compareTo(Vertex v) {
+            return this.dist - v.getDist();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        List<Vertex>[] al = new List[n + 1];
+        for(int i=1 ; i<=n ; i++) {
+            al[i] = new LinkedList<>();
+        }
+        for(int i=0 ; i<e ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            al[a].add(new Vertex(b, c));
+            al[b].add(new Vertex(a, c));
+        }
+        st = new StringTokenizer(br.readLine());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
+
+        int candidate1 = isValid(dijkstra(al, 1, v1), dijkstra(al, v1, v2), dijkstra(al, v2, n));
+        int candidate2 = isValid(dijkstra(al, 1, v2), dijkstra(al, v2, v1), dijkstra(al, v1, n));
+        if(candidate1 > 0 && candidate2 > 0) {
+            System.out.println(Math.min(candidate1, candidate2));
+        } else if(candidate1 > 0) {
+            System.out.println(candidate1);
+        } else if(candidate2 > 0) {
+            System.out.println(candidate2);
+        } else {
+            System.out.println(-1);
+        }
+
+        br.close();
+        System.exit(0);
+    }
+
+    private static int dijkstra(List<Vertex>[] al, int src, int target) {
+        int[] minDist = new int[al.length];
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+        PriorityQueue<Vertex> pq = new PriorityQueue<>();
+        pq.add(new Vertex(src, 0));
+        while(!pq.isEmpty()) {
+            Vertex v = pq.poll();
+            if(minDist[v.getTarget()] <= v.getDist()) {
+                continue;
+            }
+            minDist[v.getTarget()] = v.getDist();
+            for(Vertex cv : al[v.getTarget()]) {
+                pq.add(new Vertex(cv.getTarget(), v.getDist() + cv.getDist()));
+            }
+        }
+        return minDist[target];
+    }
+
+    private static int isValid(int minDistA, int minDistB, int minDistC) {
+        if(minDistA != Integer.MAX_VALUE
+                && minDistB != Integer.MAX_VALUE
+                && minDistC != Integer.MAX_VALUE) {
+            return minDistA + minDistB + minDistC;
+        }
+        return -1;
+    }
+}
