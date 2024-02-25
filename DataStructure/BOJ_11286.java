f776376eac33b70a6324fa9c69c9d6b989243deb
juyoungha <htj3460@gmail.com>
2024-02-25 13:26:37 +0900

    Solution[BOJ - Q.11286] : 처리시간 개선

--- DataStructure/BOJ_11286.java
+++ DataStructure/BOJ_11286.java
@@ -5,45 +5,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static class Node implements Comparable<Node> {
-        private final int value;
-
-        public Node(int value) {
-            this.value = value;
-        }
-
-        public int getValue() { return this.value; }
-
-        @Override
-        public int compareTo(Node node) {
-            if(Math.abs(this.value) != Math.abs(node.getValue())) {
-                return Integer.compare(Math.abs(this.value), Math.abs(node.getValue()));
-            } else {
-                return Integer.compare(this.value, node.getValue());
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Node> pQueue = new PriorityQueue<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(bf.readLine()); // 연산의 갯수
-        for(int i=0 ; i<n ; i++) {
-            int operation = Integer.parseInt(bf.readLine());
-            if(operation == 0) {
-                if(pQueue.isEmpty()) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(pQueue.poll().getValue());
-                }
-            } else {
-                pQueue.add(new Node(operation));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((e1, e2) -> {
+            if(Math.abs(e1) == Math.abs(e2)) {
+                return e1 - e2;
             }
+            return Math.abs(e1) - Math.abs(e2);
+        });
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0 ; i<n ; i++) {
+            int command = Integer.parseInt(br.readLine());
+            if(command == 0) {
+                if(pq.isEmpty()) {
+                    sb.append(command).append("\n");
+                    continue;
+                }
+                sb.append(pq.poll()).append("\n");
+                continue;
+            }
+            pq.add(command);
         }
 
-        bf.close();
+        System.out.println(sb);
+
+        br.close();
         System.exit(0);
     }
 }
