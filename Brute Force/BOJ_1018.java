70f67736a9822ed252b4a884a8998ce268761f9c
juyoungha <htj3460@gmail.com>
2024-07-12 18:56:46 +0900

    Solution[BOJ - Q.1018] : 솔루션 개선

--- Brute Force/BOJ_1018.java
+++ Brute Force/BOJ_1018.java
@@ -1,62 +1,54 @@
 // BOJ - 1018
 // Problem Sheet - https://www.acmicpc.net/problem/1018
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner key = new Scanner(System.in);
-		int m = key.nextInt();
-		int n = key.nextInt();
-		String tmp;
-		boolean[][] board = new boolean[m][n];
-		key.nextLine();
-		for(int i=0 ; i<m ; i++) {
-			tmp = key.nextLine();
-			for(int j=0 ; j<n ; j++)
-				board[i][j] = (tmp.charAt(j) == 'W') ? true : false;
-		}
-		int minCount = 64; // the minimum number of coloring
-		int tmpCount = 0;  // store the temporary counting result
-		boolean curColor;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		// counting logic
-		for(int i=0 ; i<m-7 ; i++) {
-			for(int j=0 ; j<n-7 ; j++) {
-				// search the available board - start point : white
-				curColor = true;
-				for(int k=i ; k<i+8 ; k++) {
-					for(int l=j ; l<j+8 ; l++) {
-						if(board[k][l] != curColor)
-							tmpCount++;
-						curColor = !curColor;
-					}
-					curColor = !curColor;
-				}
-				if(minCount > tmpCount)
-					minCount = tmpCount;
-				tmpCount = 0;
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[][] map = new boolean[n][m];
+        for (int i=0 ; i<n ; i++) {
+            String row = br.readLine();
+            for (int j=0 ; j<m ; j++) {
+                map[i][j] = (row.charAt(j) == 'W');
+            }
+        }
 
-				// search the available board - start point : black
-				curColor = false;
-				for(int k=i ; k<i+8 ; k++) {
-					for(int l=j ; l<j+8 ; l++) {
-						if(board[k][l] != curColor)
-							tmpCount++;
-						curColor = !curColor;
-					}
-					curColor = !curColor;
-				}
-				if(minCount > tmpCount)
-					minCount = tmpCount;
-				tmpCount = 0;
-			}
-		}
+        int minFixCnt = 64;
+        for (int i=0 ; i<n-7 ; i++) {
+            for (int j=0 ; j<m-7 ; j++) {
+                minFixCnt = Math.min(
+                        minFixCnt,
+                        Math.min(
+                                getFixCnt(map, true, j, i),
+                                getFixCnt(map, false, j, i)
+                        )
+                );
+            }
+        }
+        System.out.println(minFixCnt);
 
-		// print out the result
-		System.out.println(minCount);
+        br.close();
+        System.exit(0);
+    }
 
-		key.close(); // close the Scanner
-		System.exit(0);
-	}
+    private static int getFixCnt(boolean[][] map, boolean type, int x, int y) {
+        int fixCnt = 0;
+        boolean status = type;
+        for (int i=y ; i<y+8 ; i++) {
+            for (int j=x ; j<x+8 ; j++) {
+                if (map[i][j] != status) {
+                    fixCnt++;
+                }
+                status = !status;
+            }
+            status = !status;
+        }
+        return fixCnt;
+    }
 }
