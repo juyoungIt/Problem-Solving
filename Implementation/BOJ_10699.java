c7826e44c46685c5c16e62a798b82582868ded2f
juyoungha <htj3460@gmail.com>
2024-08-10 18:19:00 +0900

    Solution[BOJ - Q.10699]

---
+++ Implementation/BOJ_10699.java
@@ -0,0 +1,12 @@
+// BOJ - 10699
+// Problem Sheet - https://www.acmicpc.net/problem/10699
+
+import java.time.LocalDate;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
