ef660b859cbedbd9f0cdc219a2997af1cc9704ab
juyoungha <htj3460@gmail.com>
2024-10-10 14:12:11 +0900

    Refactor[BOJ - Q.2817] : 가독성 및 구조 개선

--- Implementation/BOJ_2817.java
+++ Implementation/BOJ_2817.java
@@ -3,48 +3,98 @@
 
 import java.util.*;
 import java.io.*;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    static class Staff {
+        private final char name;
+        private final int recommended;
+        private int chipCount;
+
+        public Staff(char name, int recommended) {
+            this.name = name;
+            this.recommended = recommended;
+            this.chipCount = 0;
+        }
+
+        public char getName() { return this.name; }
+        public int getRecommended() { return this.recommended; }
+        public void giveChip() { this.chipCount++; }
+
+        @Override
+        public String toString() {
+            return this.name + " " + this.chipCount;
+        }
+    }
+
+    static class Score implements Comparable<Score> {
+        private final Staff staff;
+        private final double value;
+
+        public Score(Staff staff, double value) {
+            this.staff = staff;
+            this.value = value;
+        }
+
+        public Staff getStaff() { return this.staff; }
+        public double getValue() { return this.value; }
+
+        @Override
+        public int compareTo(Score s) {
+            return Double.compare(s.getValue(), this.value);
+        }
+    }
+
+    private static int x;
+    private static int n;
+
     public static void main(String[] args) throws IOException {
+        List<Staff> staffs = getStaffsInfoFromInput().stream()
+                .filter(s -> (double) s.getRecommended() / x >= 0.05)
+                .collect(Collectors.toList());
+        giveChipToStaffs(getScoreQueue(staffs));
+        staffs.sort(Comparator.comparingInt(Staff::getName));
+        System.out.println(getOutput(staffs));
+    }
+
+    private static List<Staff> getStaffsInfoFromInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int x = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
-        double[][] scores = new double[14 * 26][2];
-        int[] staffs = new int[26];
-        Arrays.fill(staffs, -1);
+        x = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        List<Staff> staffs = new ArrayList<>(n);
         for (int i=0 ; i<n ; i++) {
             st = new StringTokenizer(br.readLine());
             char name = st.nextToken().charAt(0);
-            int count = Integer.parseInt(st.nextToken());
-            if ((double)count / x < 0.05) {
-                continue;
-            }
-            staffs[name - 'A'] = 0;
-            for (int j=1 ; j<=14 ; j++) {
-                scores[14 * i + j - 1][0] = name - 'A';
-                scores[14 * i + j - 1][1] = (double) count / j;
-            }
+            int recommended = Integer.parseInt(st.nextToken());
+            staffs.add(new Staff(name, recommended));
         }
-        Arrays.sort(scores, (o1, o2) -> Double.compare(o2[1], o1[1]));
-
-        for (int i=0 ; i<14 ; i++) {
-            staffs[(int)scores[i][0]]++;
-        }
-
-        for (int i=0 ; i<26 ; i++) {
-            if (staffs[i] >= 0) {
-                sb.append((char)(i + 'A'))
-                        .append(" ")
-                        .append(staffs[i])
-                        .append("\n");
-            }
-        }
-        System.out.println(sb);
 
         br.close();
-        System.exit(0);
+        return staffs;
+    }
+
+    private static PriorityQueue<Score> getScoreQueue(List<Staff> staffs) {
+        PriorityQueue<Score> scores = new PriorityQueue<>();
+        staffs.forEach(s -> {
+            for (int i=1 ; i<=14 ; i++) {
+                scores.add(new Score(s, (double) s.getRecommended() / i));
+            }
+        });
+        return scores;
+    }
+
+    private static void giveChipToStaffs(PriorityQueue<Score> scoreQueue) {
+        for (int i=0 ; i<14 ; i++) {
+            scoreQueue.poll().getStaff().giveChip();
+        }
+    }
+
+    private static String getOutput(List<Staff> staffs) {
+        StringBuilder sb = new StringBuilder();
+        staffs.forEach(s -> sb.append(s.toString()).append("\n"));
+        return sb.toString();
     }
 }
