56697cad23baef5b14f5dd5a64c5a91ae76d9e65
juyoungha <htj3460@gmail.com>
2024-10-21 13:26:05 +0900

    Refactor[BOJ - Q.2230] : 솔루션 개선

--- Two Pointer/BOJ_2230.java
+++ Two Pointer/BOJ_2230.java
@@ -6,32 +6,32 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] sequence = new int[N];
-        for(int i=0 ; i<N ; i++) {
-            sequence[i] = Integer.parseInt(bf.readLine());
+        int[] arr = new int[N];
+        for (int i=0 ; i<N ; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(sequence);
+        Arrays.sort(arr);
 
-        int rightIndex = 1;
-        int minDiff = 2000000001;
-        for(int i=0 ; i<N-1 ; i++) {
-            int diff = sequence[rightIndex] - sequence[i];
-            while(diff < M && rightIndex < N-1) {
-                diff = sequence[++rightIndex] - sequence[i];
-            }
-            if(diff >= M) {
+        int left = 0;
+        int right = 0;
+        int minDiff = 2_000_000_001;
+        while (left <= right && right < N) {
+            int diff = arr[right] - arr[left];
+            if (diff >= M) {
                 minDiff = Math.min(minDiff, diff);
+                left++;
+            } else {
+                right++;
             }
         }
 
         System.out.println(minDiff);
 
-        bf.close();
-        System.exit(0);
+        br.close();
     }
 }
