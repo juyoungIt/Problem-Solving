44a9f3ece8f26c19cdcf76d674bc1ced53d2822d
juyoungha <htj3460@gmail.com>
2024-10-30 16:34:14 +0900

    Refactor[BOJ - Q.1406] : 솔루션 코드 개선

--- DataStructure/BOJ_1406.java
+++ DataStructure/BOJ_1406.java
@@ -6,55 +6,46 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        List<Character> cache = new LinkedList<>(); // 작업한 문자열을 저장하는 공간
-        String words = bf.readLine();
-        for(int i=0 ; i<words.length() ; i++) {
-            cache.add(words.charAt(i));
+        List<Character> buffer = new LinkedList<>();
+        String data = br.readLine();
+        for (int i=0 ; i<data.length() ; i++) {
+            buffer.add(data.charAt(i));
         }
 
-        ListIterator<Character> iterator = cache.listIterator();
-        while(iterator.hasNext()) {
-            iterator.next();
-        }
-
-        int commandCnt = Integer.parseInt(bf.readLine()); // 사용한 명령어의 수
-        for(int i=0 ; i<commandCnt ; i++) {
-            String[] commands = bf.readLine().split(" ");
-            switch (commands[0]) {
-                case "L":
-                    if (iterator.hasPrevious()) {
-                        iterator.previous();
-                    }
-                    break;
-                case "D":
-                    if (iterator.hasNext()) {
-                        iterator.next();
-                    }
-                    break;
-                case "B":
-                    if(iterator.hasPrevious()) {
-                        iterator.previous();
-                        iterator.remove();
-                    }
-                    break;
-                case "P":
-                    iterator.add(commands[1].charAt(0));
-                    break;
-                default:
-                    throw new IllegalArgumentException();
+        ListIterator<Character> iter = buffer.listIterator(buffer.size());
+        int M = Integer.parseInt(br.readLine());
+        for (int i=0 ; i<M ; i++) {
+            st = new StringTokenizer(br.readLine());
+            char command = st.nextToken().charAt(0);
+            if (command == 'L') {
+                if (iter.hasPrevious()) {
+                    iter.previous();
+                }
+            } else if (command == 'D') {
+                if (iter.hasNext()) {
+                    iter.next();
+                }
+            } else if (command == 'B') {
+                if (iter.hasPrevious()) {
+                    iter.previous();
+                    iter.remove();
+                }
+            } else {
+                char letter = st.nextToken().charAt(0);
+                iter.add(letter);
             }
         }
 
-        for(char word : cache) {
-            bw.write(word);
+        for (char letter : buffer) {
+            bw.write(letter);
         }
-
         bw.flush();
+
         bw.close();
-        bf.close();
-        System.exit(0);
+        br.close();
     }
 }
