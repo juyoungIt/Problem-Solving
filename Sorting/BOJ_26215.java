87607b4a41e958bc3f39a9568af24b2bc45d6b21
juyoungha <htj3460@gmail.com>
2024-10-30 10:11:39 +0900

    Refactor[BOJ - Q.26215] : 불필요한 구현 제거

--- Sorting/BOJ_26215.java
+++ Sorting/BOJ_26215.java
@@ -5,49 +5,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static class Home implements Comparable<Home> {
-        private final int index;
-        private int snow;
-
-        public Home(int index, int snow) {
-            this.index = index;
-            this.snow = snow;
-        }
-
-        public int getIndex() { return this.index; }
-        public int getSnow() { return this.snow; }
-        public void removeSnow() { this.snow--; }
-
-        @Override
-        public int compareTo(Home h) {
-            return h.getSnow() - this.snow;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        PriorityQueue<Home> homes = new PriorityQueue<>();
+        PriorityQueue<Integer> homes = new PriorityQueue<>(Collections.reverseOrder());
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i=0 ; i<N ; i++) {
-            homes.add(new Home(i, Integer.parseInt(st.nextToken())));
-        }
+        Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .forEach(homes::add);
 
         int totalMinutes = 0;
         while (!homes.isEmpty()) {
-            Home homeA = homes.poll();
-            homeA.removeSnow();
+            int homeA = homes.poll();
+            homeA--;
             if (!homes.isEmpty()) {
-                Home homeB = homes.poll();
-                homeB.removeSnow();
-                if (homeB.getSnow() > 0) {
+                int homeB = homes.poll();
+                homeB--;
+                if (homeB > 0) {
                     homes.add(homeB);
                 }
             }
-            if (homeA.getSnow() > 0) {
+            if (homeA > 0) {
                 homes.add(homeA);
             }
             totalMinutes++;
