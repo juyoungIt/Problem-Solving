c9bfa5e122ae9e6af0f93312de98cd7857791530
juyoungha <htj3460@gmail.com>
2024-11-06 11:05:35 +0900

    Refactor[BOJ - Q.1966] : 솔루션 코드 개선

--- DataStructure/BOJ_1966.java
+++ DataStructure/BOJ_1966.java
@@ -6,64 +6,59 @@ import java.io.*;
 
 public class Main {
 
-    static class Doc implements Comparable<Doc> {
-        private final int idx; // 처음 queue에 위치한 인덱스
-        private final int weight; // 가중치 값
+    static class Document implements Comparable<Document> {
+        private final int id;
+        private final int importance;
 
-        public Doc(int idx, int weight) {
-            this.idx = idx;
-            this.weight = weight;
+        public Document(int id, int importance) {
+            this.id = id;
+            this.importance = importance;
         }
 
-        public int getIdx() { return this.idx; }
-        public int getWeight() { return this.weight; }
+        public int getId() { return this.id; }
+        public int getImportance() { return this.importance; }
 
         @Override
-        public int compareTo(Doc doc) {
-            return Integer.compare(this.weight, doc.getWeight()) * -1;
+        public int compareTo(Document d) {
+            return d.getImportance() - this.importance;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int tcSize = Integer.parseInt(bf.readLine()); // 테스트 케이스의 수
-        Deque<Doc> printSeq = new LinkedList<>(); // 프린트 순서를 저장한 queue
-        PriorityQueue<Integer> weightQueue = new PriorityQueue<>(Collections.reverseOrder()); // 가중치 저장
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st;
-        for(int i=0 ; i<tcSize ; i++) {
-            st = new StringTokenizer(bf.readLine());
-            int docCnt = Integer.parseInt(st.nextToken()); // 문서의 수
-            int target = Integer.parseInt(st.nextToken()); // 번째 수가 궁금한 문서
-            int answer = 0; // 기다린 횟수
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int N = Integer.parseInt(input[0]);
+            int M = Integer.parseInt(input[1]);
 
-            // n개의 중요도 정보를 수집함
-            st = new StringTokenizer(bf.readLine());
-            for(int j=0 ; j<docCnt ; j++) {
-                int weight = Integer.parseInt(st.nextToken());
-                weightQueue.add(weight);
-                printSeq.addLast(new Doc(j, weight));
+            Queue<Document> queue = new LinkedList<>();
+            Document[] documents = new Document[N];
+            input = br.readLine().split(" ");
+            for (int i=0 ; i<N ; i++) {
+                documents[i] = new Document(i, Integer.parseInt(input[i]));
+                queue.add(documents[i]);
             }
+            Arrays.sort(documents);
 
-            // 목표로 하는 문서가 출력가능할 때까지 다음을 반복
-            while(true) {
-                if(printSeq.getFirst().getWeight() < weightQueue.peek())
-                    printSeq.addLast(printSeq.pollFirst());
-                else {
-                    if(printSeq.getFirst().getIdx() == target)
+            int curSeq = 0;
+            while (!queue.isEmpty()) {
+                if (queue.peek().getImportance() == documents[curSeq].getImportance()) {
+                    Document printed = queue.poll();
+                    curSeq++;
+                    if (printed.getId() == M) {
                         break;
-                    answer++;
-                    weightQueue.poll();
-                    printSeq.removeFirst();
+                    }
+                } else {
+                    queue.add(queue.poll());
                 }
             }
-
-            System.out.println(answer+1);
-            printSeq.clear(); // 재사용을 위해 초기화
-            weightQueue.clear(); // 재사용을 위해 초기화
+            sb.append(curSeq).append("\n");
         }
 
-        bf.close();
-        System.exit(0);
+        System.out.println(sb);
+        br.close();
     }
 }
