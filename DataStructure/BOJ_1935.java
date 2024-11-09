bf0e15eb7ffd72882bd1482320e74ec91e5b11ef
juyoungha <htj3460@gmail.com>
2024-11-10 00:34:02 +0900

    Refactor[BOJ - Q.1935] : 솔루션 코드 개선

--- DataStructure/BOJ_1935.java
+++ DataStructure/BOJ_1935.java
@@ -6,43 +6,52 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Double> stack = new Stack<>();
-        Map<Character, Double> valueMapper = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bf.readLine()); // 피연산자의 갯수
-        char[] expression = bf.readLine().toCharArray();
-        for(int i=1 ; i<=n ; i++) {
-            valueMapper.put((char)(64+i), Double.parseDouble(bf.readLine()));
+        int N = Integer.parseInt(br.readLine());
+        char[] expression = br.readLine().toCharArray();
+        int[] operands = new int[N];
+        for (int i=0 ; i<N ; i++) {
+            operands[i] = Integer.parseInt(br.readLine());
         }
 
-        double operand1;
-        double operand2;
-        for(char element : expression) {
-            if(isOperator(element)) { // 연산자인 경우
-                operand1 = stack.pop();
-                operand2 = stack.pop();
-                if(element == '+') {
-                    stack.push(operand2 + operand1);
-                } else if(element == '-') {
-                    stack.push(operand2 - operand1);
-                } else if(element == '*') {
-                    stack.push(operand2 * operand1);
-                } else {
-                    stack.push(operand2 / operand1);
+        Deque<Double> stack = new ArrayDeque<>(N);
+        for (char element : expression) {
+            if (isOperator(element)) {
+                switch (element) {
+                    case '+':
+                        double operandA = stack.pop();
+                        double operandB = stack.pop();
+                        stack.push(operandB + operandA);
+                        break;
+                    case '-':
+                        operandA = stack.pop();
+                        operandB = stack.pop();
+                        stack.push(operandB - operandA);
+                        break;
+                    case '*':
+                        operandA = stack.pop();
+                        operandB = stack.pop();
+                        stack.push(operandB * operandA);
+                        break;
+                    case '/':
+                        operandA = stack.pop();
+                        operandB = stack.pop();
+                        stack.push(operandB / operandA);
+                        break;
+                    default:
+                        throw new IllegalArgumentException();
                 }
-            } else { // 피연산자인 경우
-                stack.push(valueMapper.get(element));
+                continue;
             }
+            stack.push((double) operands[(element - 'A')]);
         }
 
-        System.out.printf("%.2f", stack.pop());
-
-        bf.close();
-        System.exit(0);
+        System.out.printf("%.2f\n", stack.pop());
+        br.close();
     }
 
-    static boolean isOperator(char element) {
+    private static boolean isOperator(char element) {
         return element == '+' || element == '-' || element == '*' || element == '/';
     }
 }
