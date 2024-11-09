1b50a7423441698df966215eebc1f7a4c69a4973
juyoungha <htj3460@gmail.com>
2024-11-09 19:46:36 +0900

    Refactor[BOJ - Q.17413] : 솔루션 코드 개선

--- DataStructure/BOJ_17413.java
+++ DataStructure/BOJ_17413.java
@@ -6,43 +6,42 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        char[] letters = bf.readLine().toCharArray();
-        Stack<Character> stack = new Stack<>(); // 문자열을 반전시키기 위한 stack
-        StringBuilder sb = new StringBuilder(); // 문자열 반전 결과를 저장
-        boolean isTag = false; // 현재 태그를 참조 중인 가에 대한 여부
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        for(int i=0 ; i<letters.length ; i++) {
-            char letter = letters[i]; // 문자열에서 한 개의 문자를 가져옴
-            if(!isTag) {
-                if(letter == '<') {
+        Deque<Character> stack = new ArrayDeque<>();
+        boolean isTag = false;
+        for (char letter : br.readLine().toCharArray()) {
+            switch (letter) {
+                case '<':
                     isTag = true;
-                    while(!stack.isEmpty())
-                        sb.append(stack.pop());
+                    while (!stack.isEmpty()) {
+                        sb.append(stack.pollLast());
+                    }
                     sb.append(letter);
-                }
-                else if(letter == ' ' || i == letters.length-1) {
-                    if(letter != ' ')
-                        stack.push(letter);
-                    while(!stack.isEmpty())
-                        sb.append(stack.pop());
-                    if(letter == ' ')
-                        sb.append(letter);
-                }
-                else {
-                    stack.push(letter);
-                }
-            }
-            else {
-                sb.append(letter);
-                if(letter == '>')
+                    break;
+                case '>':
                     isTag = false;
+                    sb.append(letter);
+                    break;
+                case ' ':
+                    if (!isTag) {
+                        while (!stack.isEmpty()) {
+                            sb.append(stack.pollLast());
+                        }
+                    }
+                    sb.append(letter);
+                    break;
+                default:
+                    if (isTag) sb.append(letter);
+                    else stack.offerLast(letter);
             }
+        }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollLast());
         }
 
         System.out.println(sb);
-
-        bf.close();
-        System.exit(0);
+        br.close();
     }
 }
