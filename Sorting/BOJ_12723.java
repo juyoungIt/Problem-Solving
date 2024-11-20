899e3b339421fe7618fc6adde53b8ddbdd985c4d
juyoungha <htj3460@gmail.com>
2024-11-20 18:00:08 +0900

    Refactor[BOJ - Q.12723] : 오탈자 수정 및 불필요 코드 제거

--- Sorting/BOJ_12723.java
+++ Sorting/BOJ_12723.java
@@ -6,7 +6,6 @@ import java.io.*;
 
 public class Main {
 
-    private static final int MAX_VALUE = 1_001;
     private static final String MESSAGE_FORMAT = "Case #%d: %d\n";
 
     public static void main(String[] args) throws IOException {
@@ -16,17 +15,17 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         for (int i=1 ; i<=T ; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] vertorA = Arrays.stream(br.readLine().split(" "))
+            int[] vectorA = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .sorted()
                     .toArray();
-            int[] vertorB = Arrays.stream(br.readLine().split(" "))
+            int[] vectorB = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .sorted()
                     .toArray();
             int minProduct = 0;
             for (int j=0 ; j<n ; j++) {
-                minProduct += vertorA[j] * vertorB[n - 1 - j];
+                minProduct += vectorA[j] * vectorB[n - 1 - j];
             }
             sb.append(String.format(MESSAGE_FORMAT, i, minProduct));
         }
