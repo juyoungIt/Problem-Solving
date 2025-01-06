cafb3898826004c21f915721200624d9e5089d59
juyoungha <htj3460@gmail.com>
2025-01-06 16:51:00 +0900

    Refactor[BOJ - Q.3085] : 솔루션 코드 개선

--- Brute Force/BOJ_3085.java
+++ Brute Force/BOJ_3085.java
@@ -5,73 +5,83 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int maxLength = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int sizeOfBoard = Integer.parseInt(bf.readLine());
-        char[][] board = new char[sizeOfBoard][sizeOfBoard];
-        for(int i=0 ; i<sizeOfBoard ; i++) {
-            board[i] = bf.readLine().toCharArray();
-        }
-
-        for(int i=0 ; i<sizeOfBoard ; i++) {
-            for(int j=0 ; j<sizeOfBoard ; j++) {
-                if(j+1<sizeOfBoard && board[i][j] != board[i][j+1]) {
-                    swap(board, i, j, i, j+1);
-                    maxLength = Math.max(maxLength, Math.max(getRowMax(board), getColumnMax(board)));
-                    swap(board, i, j, i, j+1);
-                }
-                if(i+1<sizeOfBoard && board[i][j] != board[i+1][j]) {
-                    swap(board, i, j, i+1, j);
-                    maxLength = Math.max(maxLength, Math.max(getRowMax(board), getColumnMax(board)));
-                    swap(board, i, j, i+1, j);
-                }
+        int N = Integer.parseInt(br.readLine());
+        char[][] board = new char[N][N];
+        for (int i=0 ; i<N ; i++) {
+            String row = br.readLine();
+            for (int j=0 ; j<N ; j++) {
+                board[i][j] = row.charAt(j);
             }
         }
 
-        System.out.println(maxLength);
-
-        bf.close();
-        System.exit(0);
+        System.out.println(Math.max(getTotalRowMaxLen(board, N), getTotalColMaxLen(board, N)));
+        br.close();
     }
 
     private static void swap(char[][] board, int x1, int y1, int x2, int y2) {
-        char tmp = board[x1][y1];
-        board[x1][y1] = board[x2][y2];
-        board[x2][y2] = tmp;
+        char tmp = board[y1][x1];
+        board[y1][x1] = board[y2][x2];
+        board[y2][x2] = tmp;
     }
 
-    private static int getRowMax(char[][] board) {
-        int maxLength = 0;
-        for(int i=0 ; i<board.length ; i++) {
-            int curLength = 1;
-            for(int j=1 ; j<board.length ; j++) {
-                if(board[i][j] == board[i][j-1]) {
-                    curLength++;
-                } else {
-                    maxLength = Math.max(curLength, maxLength);
-                    curLength = 1;
-                }
+    private static int getTotalRowMaxLen(char[][] board, int N) {
+        int maxLen = 0;
+        for (int i=0 ; i<N ; i++) {
+            for (int j=0 ; j<N-1 ; j++) {
+                swap(board, j, i, j + 1, i);
+                maxLen = Math.max(maxLen, getRowMaxLen(board, N, i));
+                maxLen = Math.max(maxLen, getColMaxLen(board, N, j));
+                maxLen = Math.max(maxLen, getColMaxLen(board, N, j + 1));
+                swap(board, j, i, j + 1, i);
             }
-            maxLength = Math.max(curLength, maxLength);
         }
-        return maxLength;
+        return maxLen;
     }
 
-    private static int getColumnMax(char[][] board) {
-        int maxLength = 0;
-        for(int j=0 ; j<board.length ; j++) {
-            int curLength = 1;
-            for(int i=1 ; i<board.length ; i++) {
-                if(board[i][j] == board[i-1][j]) {
-                    curLength++;
-                } else {
-                    maxLength = Math.max(curLength, maxLength);
-                    curLength = 1;
-                }
+    private static int getTotalColMaxLen(char[][] board, int N) {
+        int maxLen = 0;
+        for (int i=0 ; i<N ; i++) {
+            for (int j=0 ; j<N-1 ; j++) {
+                swap(board, i, j, i, j + 1);
+                maxLen = Math.max(maxLen, getColMaxLen(board, N, i));
+                maxLen = Math.max(maxLen, getRowMaxLen(board, N, j));
+                maxLen = Math.max(maxLen, getRowMaxLen(board, N, j + 1));
+                swap(board, i, j, i, j + 1);
             }
-            maxLength = Math.max(curLength, maxLength);
         }
-        return maxLength;
+        return maxLen;
     }
+
+    private static int getRowMaxLen(char[][] board, int N, int rowIdx) {
+        int maxLen = 0;
+        int curLen = 1;
+        for (int i=1 ; i<N ; i++) {
+            if (board[rowIdx][i - 1] == board[rowIdx][i]) {
+                curLen++;
+            } else {
+                maxLen = Math.max(maxLen, curLen);
+                curLen = 1;
+            }
+        }
+        maxLen = Math.max(maxLen, curLen);
+        return maxLen;
+    }
+
+    private static int getColMaxLen(char[][] board, int N, int colIdx) {
+        int maxLen = 0;
+        int curLen = 1;
+        for (int i=1 ; i<N ; i++) {
+            if (board[i - 1][colIdx] == board[i][colIdx]) {
+                curLen++;
+            } else {
+                maxLen = Math.max(maxLen, curLen);
+                curLen = 1;
+            }
+        }
+        maxLen = Math.max(maxLen, curLen);
+        return maxLen;
+    }
+
 }
