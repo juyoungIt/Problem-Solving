bd777609fb0c8bd845286a95f11d13094806d9d2
juyoungha <htj3460@gmail.com>
2025-01-17 17:08:50 +0900

    Refactor[BOJ - Q.1339] : 솔루션 코드 개선

--- Greedy/BOJ_1339.java
+++ Greedy/BOJ_1339.java
@@ -1,63 +1,47 @@
 // BOJ - 1339
 // Problem Sheet - https://www.acmicpc.net/problem/1339
 
+import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    private static int N;
+    private static String[] words;
+    private static final int[] weights = new int[26];
+
     public static void main(String[] args) throws IOException {
-		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-		int[] alpha = new int[26]; // 수집된 알파벳 정보를 저장
-
-		int n = Integer.parseInt(bf.readLine()); // 문자열의 수
-		String[] str = new String[n]; // 입력되는 문자열을 저장
-		int answer = 0;
-		int value = 9;
-		
-		// 문자열 입력
-		for(int i=0 ; i<n ; i++)
-			str[i] = bf.readLine();
-		
-		// 문자열을 구성하는 각 문자에 대한 정보를 수집함
-		for(int i=0 ; i<n ; i++)
-			for(int j=0 ; j<str[i].length() ; j++)
-				alpha[str[i].charAt(j)-65] += (int)Math.pow(10, str[i].length()-j-1);
-
-		// 수집된 정보를 기반으로 최대합을 도출함
-		int idx = 0;
-		while(!isFinish(alpha)) {
-			idx = getBigIdx(alpha);
-			answer += alpha[idx] * value;
-			value--;
-			alpha[idx] = 0;
-		}
-
-		System.out.println(answer);
-
-		bf.close();
-		System.exit(0);
+        input();
+        System.out.println(solve());
     }
 
-	// 모든 alpha에 대한 값이 처리되었는 지 확인함.
-	public static boolean isFinish(int[] arr) {
-		for(int i=0 ; i<arr.length ; i++)
-			if(arr[i] > 0)
-				return false;
-		return true;
-	}
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        words = new String[N];
+        for (int i=0 ; i<N ; i++) {
+            words[i] = br.readLine();
+        }
+        br.close();
+    }
 
-	// 가장 큰 수의 인덱스를 구함
-	public static int getBigIdx(int[] arr) {
-		int max_val = 0;
-		int max_idx = 0;
-		
-		for(int i=0 ; i<arr.length ; i++) {
-			if(max_val < arr[i]) {
-				max_val = arr[i];
-				max_idx = i;
-			}
-		}
-		return max_idx;
-	}
+    private static int solve() {
+        int maxSum = 0;
+        updateLetterWeight();
+        Arrays.sort(weights);
+        int base = 9;
+        for (int i=25 ; i>=0 ; i--) {
+            if (weights[i] <= 0) break;
+            maxSum += base-- * weights[i];
+        }
+        return maxSum;
+    }
+
+    private static void updateLetterWeight() {
+        for (String word : words) {
+            for (int i=0 ; i<word.length() ; i++) {
+                weights[word.charAt(i) - 'A'] += (int) Math.pow(10, word.length() - i - 1);
+            }
+        }
+    }
 }
