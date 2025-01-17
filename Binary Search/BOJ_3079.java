e9214c306a00bab5b17397fac65a9c57d4776089
juyoungha <htj3460@gmail.com>
2025-01-17 16:29:39 +0900

    Refactor[BOJ - Q.3079] : 솔루션 코드 개선

--- Binary Search/BOJ_3079.java
+++ Binary Search/BOJ_3079.java
@@ -1,48 +1,53 @@
 // BOJ - 3079
 // Problem Sheet - https://www.acmicpc.net/problem/3079
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    private static int N, M;
+    private static int[] T;
+    private static int maxT = 0;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int[] times = new int[N];
-        int M = Integer.parseInt(st.nextToken());
-        int minTime = Integer.MAX_VALUE;
-        for(int i=0 ; i<N ; i++) {
-            times[i] = Integer.parseInt(bf.readLine());
-            minTime = Math.min(minTime, times[i]);
-        }
-
-        long start = 0L;
-        long end = (long)minTime * M;
-        long middle;
-        long answer = end;
-        while(start <= end) {
-            middle = (start + end) / 2;
-            if(getServicedCount(times, middle) >= M) {
-                answer = middle;
-                end = middle - 1;
-            } else {
-                start = middle + 1;
-            }
-        }
-
-        System.out.println(answer);
-
-        bf.close();
-        System.exit(0);
+        input();
+        System.out.println(solve());
     }
 
-    private static long getServicedCount(int[] times, long totalTime) {
-        long servicedCount = 0;
-        for(int time : times) {
-            servicedCount += totalTime / time;
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] row = br.readLine().split(" ");
+        N = Integer.parseInt(row[0]);
+        M = Integer.parseInt(row[1]);
+        T = new int[N];
+        for (int i=0 ; i<N ; i++) {
+            T[i] = Integer.parseInt(br.readLine());
+            maxT = Math.max(maxT, T[i]);
         }
-        return servicedCount;
+        br.close();
+    }
+
+    private static long solve() {
+        long start = 0, end = (long) maxT * M;
+        long minTime = end;
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            if (isAvailable(mid)) {
+                minTime = Math.min(minTime, mid);
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return minTime;
+    }
+
+    private static boolean isAvailable(long time) {
+        long count = 0;
+        for (int t : T) {
+            count += time / t;
+            if (count >= M) return true;
+        }
+        return false;
     }
 }
