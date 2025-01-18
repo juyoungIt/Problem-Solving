e91a28fdc07aa7adfe78fcd4bfc738ec80dd2070
juyoungha <htj3460@gmail.com>
2025-01-18 23:30:07 +0900

    Refactor[BOJ - Q.15961] : 솔루션 코드 개선 (HashSet 제거)

--- Sliding Window/BOJ_15961.java
+++ Sliding Window/BOJ_15961.java
@@ -1,7 +1,6 @@
 // BOJ - 15961
 // Problem Sheet - https://www.acmicpc.net/problem/15961
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
@@ -29,22 +28,25 @@ public class Main {
     }
 
     private static int solve() {
-        int maxKinds = 0;
+        int maxKinds = 0, kinds = 0;
         int[] counts = new int[d + 1];
-        Set<Integer> kindSet = new HashSet<>();
         for (int i=0 ; i<k ; i++) {
             counts[arr[i]]++;
-            kindSet.add(arr[i]);
+            if (counts[arr[i]] == 1) {
+                kinds++;
+            }
         }
-        maxKinds = Math.max(maxKinds, kindSet.size() + (kindSet.contains(c) ? 0 : 1));
+        maxKinds = Math.max(maxKinds, kinds + (counts[c] > 0 ? 0 : 1));
         for (int i=1 ; i<N ; i++) {
             counts[arr[i - 1]]--;
             if (counts[arr[i - 1]] == 0) {
-                kindSet.remove(arr[i - 1]);
+                kinds--;
             }
             counts[arr[(i + k - 1) % N]]++;
-            kindSet.add(arr[(i + k - 1) % N]);
-            maxKinds = Math.max(maxKinds, kindSet.size() + (kindSet.contains(c) ? 0 : 1));
+            if (counts[arr[(i + k - 1) % N]] == 1) {
+                kinds++;
+            }
+            maxKinds = Math.max(maxKinds, kinds + (counts[c] > 0 ? 0 : 1));
         }
         return maxKinds;
     }
