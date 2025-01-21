b00c971bd9beff07a022f3a91ad14e9e6ba19f05
juyoungha <htj3460@gmail.com>
2025-01-21 15:23:35 +0900

    Refactor[BOJ - Q.2504] : 솔루션 코드 개선

--- DataStructure/BOJ_2504.java
+++ DataStructure/BOJ_2504.java
@@ -6,82 +6,73 @@ import java.io.*;
 
 public class Main {
 
-    private static final int SMALL_PARENTHESES = 2;
-    private static final int LARGE_PARENTHESES = 3;
+    private static String[] letters;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int totalValue = 0; // 괄호에 대한 값
-        boolean isValid = true; // 입력된 문자열이 유효한 문자열인가?
-
-        Stack<String> stack = new Stack<>();
-        String[] parentheses = bf.readLine().split(""); // 입력 문자열
-        for(String paren : parentheses) {
-            if(paren.equals("[") || paren.equals("(")) { // 여는 괄호
-                stack.add(paren);
-            } else { // 닫는 괄호
-                if(stack.isEmpty()) {
-                    isValid = false;
-                    break;
-                }
-                int partValue = 0; // 괄호 부분에 대한 값의 합
-                boolean reachedPair = false;
-                while(!stack.isEmpty()) {
-                    if(stack.peek().equals(getPair(paren))) {
-                        reachedPair = true;
-                        break;
-                    }
-                    if(stack.peek().equals("[") || stack.peek().equals("(")) {
-                        isValid = false;
-                        break;
-                    }
-                    partValue += Integer.parseInt(stack.pop());
-                }
-                if(!reachedPair) {
-                    isValid = false;
-                    break;
-                }
-                if(!isValid) {
-                    break;
-                }
-                if(!stack.isEmpty()) {
-                    stack.pop();
-                }
-                if(partValue == 0) {
-                    if(paren.equals("]")) {
-                        stack.push(Integer.toString(LARGE_PARENTHESES));
-                    } else {
-                        stack.push(Integer.toString(SMALL_PARENTHESES));
-                    }
-                } else {
-                    if(paren.equals("]")) {
-                        stack.push(Integer.toString(partValue * LARGE_PARENTHESES));
-                    } else {
-                        stack.push(Integer.toString(partValue * SMALL_PARENTHESES));
-                    }
-                }
-            }
-        }
-
-        while(!stack.isEmpty()) {
-            if(stack.peek().equals("[") || stack.peek().equals("(")) {
-                isValid = false;
-                break;
-            }
-            totalValue += Integer.parseInt(stack.pop());
-        }
-        System.out.println((isValid) ? totalValue : 0);
-
-        bf.close();
-        System.exit(0);
+        input();
+        System.out.println(solve());
     }
 
-    // 해당 닫는 괄호에 매칭되는 여는 괄호를 반환함
-    static String getPair(String closeParen) {
-        if(closeParen.equals("]")) {
-            return "[";
-        } else {
-            return "(";
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        letters = br.readLine().split("");
+        br.close();
+    }
+
+    private static int solve() {
+        int totalSum = 0;
+        Stack<String> stack = new Stack<>();
+        for (String letter : letters) {
+            if (isOpen(letter.charAt(0))) {
+                stack.push(letter);
+            } else {
+                if (stack.isEmpty()) return 0;
+                int innerSum = 0;
+                while (!stack.isEmpty()) {
+                    String top = stack.pop();
+                    if (isNumeric(top)) {
+                        innerSum += Integer.parseInt(top);
+                    } else {
+                        int matchedCode = isMatched(top.charAt(0), letter.charAt(0));
+                        if (matchedCode < 0) return 0;
+                        innerSum = Math.max(innerSum, 1);
+                        innerSum *= matchedCode;
+                        stack.push(Integer.toString(innerSum));
+                        break;
+                    }
+                }
+                if (stack.empty()) return 0;
+            }
         }
+
+        while (!stack.isEmpty()) {
+            String top = stack.pop();
+            if (!isNumeric(top)) {
+                return 0;
+            }
+            totalSum += Integer.parseInt(top);
+        }
+        return totalSum;
+    }
+
+    private static boolean isOpen(char letter) {
+        return letter == '(' || letter == '[';
+    }
+
+    private static int isMatched(char open, char close) {
+        if (open == '(' && close == ')' || open == '[' && close == ']') {
+            if (open == '(') return 2;
+            else return 3;
+        }
+        return -1;
+    }
+
+    private static boolean isNumeric(String str) {
+        for (int i=0; i<str.length() ; i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
