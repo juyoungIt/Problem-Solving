62f955a271d6f68d754ecf4bcacb97bbb1beed62
juyoungha <htj3460@gmail.com>
2025-01-23 16:55:33 +0900

    Solution[BOJ - Q.15657]

---
+++ Recursion/BOJ_15657.java
@@ -0,0 +1,54 @@
+// BOJ - 15657
+// Problem Sheet - https://www.acmicpc.net/problem/15657
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int N, M;
+    private static int[] arr;
+    private static int[] buffer;
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        generateSequence(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] row = br.readLine().split(" ");
+        N = Integer.parseInt(row[0]);
+        M = Integer.parseInt(row[1]);
+        arr = new int[N];
+        row = br.readLine().split(" ");
+        for (int i=0 ; i<N ; i++) {
+            arr[i] = Integer.parseInt(row[i]);
+        }
+        Arrays.sort(arr);
+        buffer = new int[M];
+        sb = new StringBuilder();
+        br.close();
+    }
+
+    private static void generateSequence(int depth, int startIndex) {
+        if (depth == M) {
+            sb.append(getBufferString()).append("\n");
+            return;
+        }
+        for (int i=startIndex ; i<N ; i++) {
+            buffer[depth] = arr[i];
+            generateSequence(depth + 1, i);
+        }
+    }
+
+    private static String getBufferString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0 ; i<M ; i++) {
+            sb.append(buffer[i]).append(" ");
+        }
+        return sb.toString();
+    }
+}
