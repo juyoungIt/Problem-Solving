d34d1cd2fcafb2f64d96bb67fe3545349217091e
juyoungha <htj3460@gmail.com>
2025-01-24 23:57:34 +0900

    Refactor[BOJ - Q.1068] : 솔루션 코드 개선

--- Tree/BOJ_1068.java
+++ Tree/BOJ_1068.java
@@ -5,49 +5,58 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    private static int N, X;
+    private static int[] parents;
+    private static List<Integer>[] al;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        boolean[] isRemoved = new boolean[n];
-        List<List<Integer>> al = new ArrayList<>(n);
-        for(int i=0 ; i<n ; i++) {
-            al.add(new LinkedList<>());
-        }
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0 ; i<n ; i++) {
-            int p = Integer.parseInt(st.nextToken());
-            if(p == -1) {
-                continue;
-            }
-            al.get(p).add(i);
-        }
-
-        int r = Integer.parseInt(br.readLine());
-        remove(al, isRemoved, r);
-
-        int leafNodeCnt = 0;
-        for(int i=0 ; i<n ; i++) {
-            if(!isRemoved[i] && al.get(i).isEmpty()) {
-                leafNodeCnt++;
-            }
-        }
-
-        System.out.println(leafNodeCnt);
-
-        br.close();
-        System.exit(0);
+        input();
+        System.out.println(solve());
     }
 
-    private static void remove(List<List<Integer>> al, boolean[] isRemoved, int r) {
-        isRemoved[r] = true;
-        for(List<Integer> nodes : al) {
-            nodes.remove(Integer.valueOf(r));
-            for(int node : al.get(r)) {
-                remove(al, isRemoved, node);
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        al = new LinkedList[N];
+        parents = new int[N];
+        for (int i=0 ; i<N ; i++) {
+            al[i] = new LinkedList<>();
+        }
+
+        String[] row = br.readLine().split(" ");
+        for (int i=0 ; i<N ; i++) {
+            parents[i] = Integer.parseInt(row[i]);
+            if (parents[i] < 0) continue;
+            al[parents[i]].add(i);
+        }
+
+        X = Integer.parseInt(br.readLine());
+        br.close();
+    }
+
+    private static int solve() {
+        if (parents[X] >= 0) {
+            al[parents[X]].remove((Integer) X);
+        }
+        deleteNode(X);
+        return getLeafNodeCount();
+    }
+
+    private static void deleteNode(int node) {
+        for (int child : al[node]) {
+            deleteNode(child);
+        }
+        al[node] = null;
+    }
+
+    private static int getLeafNodeCount() {
+        int leafNodeCount = 0;
+        for (int i=0 ; i<N ; i++) {
+            if (al[i] != null && al[i].isEmpty()) {
+                leafNodeCount++;
             }
         }
-        al.get(r).clear();
+        return leafNodeCount;
     }
 }
