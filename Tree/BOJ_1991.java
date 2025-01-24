963e021d767e5909566e19786ede2e6f494db86a
juyoungha <htj3460@gmail.com>
2025-01-24 18:09:09 +0900

    Refactor[BOJ - Q.1991] : 솔루션 코드 개선

--- Tree/BOJ_1991.java
+++ Tree/BOJ_1991.java
@@ -1,108 +1,74 @@
 // BOJ - 1991
 // Problem Sheet - https://www.acmicpc.net/problem/1991
 
+import java.util.*;
 import java.io.*;
 
 public class Main {
 
     private static int N;
-    private static Node[] bt;
-    private static StringBuilder buffer;
-
-    static class Node {
-        private final char name;
-        private Node leftChild;
-        private Node rightChild;
-
-        public Node(char name) {
-            this.name = name;
-        }
-
-        public char getName() { return this.name; }
-        public Node getLeftChild() { return this.leftChild; }
-        public Node getRightChild() { return this.rightChild; }
-        public void setLeftChild(Node leftChild) { this.leftChild = leftChild; }
-        public void setRightChild(Node rightChild) { this.rightChild = rightChild; }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "name=" + name +
-                    ", leftChild=" + leftChild +
-                    ", rightChild=" + rightChild +
-                    '}';
-        }
-    }
+    private static int[][] bt;
+    private static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         input();
-        System.out.println(solve());
+        solve();
+        System.out.println(sb);
     }
 
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        bt = new Node[N];
+        bt = new int[N][2];
+        for (int i=0 ; i<N ; i++) {
+            Arrays.fill(bt[i], -1);
+        }
         for (int i=0 ; i<N ; i++) {
             String[] row = br.readLine().split(" ");
             char parent = row[0].charAt(0);
-            char leftChild = row[1].charAt(0);
-            char rightChild = row[2].charAt(0);
-            if (bt[parent - 'A'] == null) {
-                bt[parent - 'A'] = new Node(parent);
-            }
-            if (leftChild != '.') {
-                if (bt[leftChild - 'A'] == null) {
-                    bt[leftChild - 'A'] = new Node(leftChild);
-                }
-                bt[parent - 'A'].setLeftChild(bt[leftChild - 'A']);
-            }
-            if (rightChild != '.') {
-                if (bt[rightChild - 'A'] == null) {
-                    bt[rightChild - 'A'] = new Node(rightChild);
-                }
-                bt[parent - 'A'].setRightChild(bt[rightChild - 'A']);
-            }
+            char left = row[1].charAt(0);
+            char right = row[2].charAt(0);
+            if (left != '.') bt[parent - 'A'][0] = left - 'A';
+            if (right != '.') bt[parent - 'A'][1] = right - 'A';
         }
-        buffer = new StringBuilder();
+        sb = new StringBuilder();
         br.close();
     }
 
-    private static String solve() {
-        preorderTraversal(bt[0]);
-        buffer.append("\n");
-        inorderTraversal(bt[0]);
-        buffer.append("\n");
-        postorderTraversal(bt[0]);
-        buffer.append("\n");
-        return buffer.toString();
+    private static void solve() {
+        preorder(0);
+        sb.append("\n");
+        inorder(0);
+        sb.append("\n");
+        postorder(0);
+        sb.append("\n");
     }
 
-    private static void preorderTraversal(Node root) {
-        if (root == null) {
+    private static void preorder(int root) {
+        if (root < 0) {
             return;
         }
-        buffer.append(root.getName());
-        preorderTraversal(root.getLeftChild());
-        preorderTraversal(root.getRightChild());
+        sb.append((char) (root + 'A'));
+        preorder(bt[root][0]);
+        preorder(bt[root][1]);
     }
 
-    private static void inorderTraversal(Node root) {
-        if (root == null) {
+    private static void inorder(int root) {
+        if (root < 0) {
             return;
         }
-        inorderTraversal(root.getLeftChild());
-        buffer.append(root.getName());
-        inorderTraversal(root.getRightChild());
+        inorder(bt[root][0]);
+        sb.append((char) (root + 'A'));
+        inorder(bt[root][1]);
     }
 
-    private static void postorderTraversal(Node root) {
-        if (root == null) {
+    private static void postorder(int root) {
+        if (root < 0) {
             return;
         }
-        postorderTraversal(root.getLeftChild());
-        postorderTraversal(root.getRightChild());
-        buffer.append(root.getName());
+        postorder(bt[root][0]);
+        postorder(bt[root][1]);
+        sb.append((char) (root + 'A'));
     }
 
 }
