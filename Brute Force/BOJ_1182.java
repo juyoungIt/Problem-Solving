69e4f274481c6d3a4da36b0456267dc96fcbab9d
juyoungha <htj3460@gmail.com>
2025-02-03 17:07:34 +0900

    Refactor[BOJ - Q.1182] : 솔루션 코드 개선

--- Brute Force/BOJ_1182.java
+++ Brute Force/BOJ_1182.java
@@ -1,46 +1,42 @@
 // BOJ - 1182
 // Problem Sheet - https://www.acmicpc.net/problem/1182
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
 
-    private static int N;
-    private static int S;
+    private static int N, S;
     private static int[] arr;
-    private static int count = 0;
+    private static int sum = 0;
+    private static int answer = 0;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        st = new StringTokenizer(bf.readLine());
-        for(int i=0 ; i<N ; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for(int i=0 ; i<N ; i++) {
-            searchTargetSum(i, arr[i]);
-        }
-
-        System.out.println(count);
-
-        bf.close();
-        System.exit(0);
+        input();
+        checkSeqSum(0, 0);
+        System.out.println(answer);
     }
 
-    private static void searchTargetSum(int startIdx, int acc) {
-        if(acc == S) {
-            count++;
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] row = br.readLine().split(" ");
+        N = Integer.parseInt(row[0]);
+        S = Integer.parseInt(row[1]);
+        arr = new int[N];
+        row = br.readLine().split(" ");
+        for (int i=0 ; i<N ; i++) {
+            arr[i] = Integer.parseInt(row[i]);
         }
-        for(int i=startIdx+1 ; i<N ; i++) {
-            acc += arr[i];
-            searchTargetSum(i, acc);
-            acc -= arr[i];
+        br.close();
+    }
+
+    private static void checkSeqSum(int depth, int startIndex) {
+        if (depth > 0 && sum == S) {
+            answer++;
+        }
+        for (int i=startIndex ; i<N ; i++) {
+            sum += arr[i];
+            checkSeqSum(depth + 1, i + 1);
+            sum -= arr[i];
         }
     }
 }
