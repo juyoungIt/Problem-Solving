9a01f868444d8f1bcfa49b925a032a7bb0178ce3
juyoungha <htj3460@gmail.com>
2025-02-05 14:04:11 +0900

    Solution[BOJ - Q.16987]

---
+++ Brute Force/BOJ_16987.java
@@ -0,0 +1,69 @@
+// BOJ - 16987
+// Problem Sheet - https://www.acmicpc.net/problem/16987
+
+import java.io.*;
+
+public class Main {
+
+    static class Egg {
+        private final int durability;
+        private final int weight;
+        private int curDurability;
+
+        public Egg(int durability, int weight) {
+            this.durability = durability;
+            this.weight = weight;
+            this.curDurability = durability;
+        }
+
+        public int getWeight() { return this.weight; }
+        public boolean isBroken() { return curDurability <= 0; }
+        public void hit(int weight) { this.curDurability -= weight; }
+        public void restore(int weight) { this.curDurability += weight; }
+    }
+
+    private static int N;
+    private static Egg[] eggs;
+    private static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve(0, 0);
+        System.out.println(answer);
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        eggs = new Egg[N];
+        for (int i=0 ; i<N ; i++) {
+            String[] row = br.readLine().split(" ");
+            int S = Integer.parseInt(row[0]);
+            int W = Integer.parseInt(row[1]);
+            eggs[i] = new Egg(S, W);
+        }
+        br.close();
+    }
+
+    private static void solve(int selected, int brokenCount) {
+        if (selected >= N) {
+            answer = Math.max(answer, brokenCount);
+            return;
+        }
+        for (int i=0 ; i<N ; i++) {
+            if (i == selected) continue;
+            if (!eggs[selected].isBroken() && !eggs[i].isBroken()) {
+                eggs[i].hit(eggs[selected].getWeight());
+                eggs[selected].hit(eggs[i].getWeight());
+                int newBrokenCount = brokenCount;
+                newBrokenCount += eggs[i].isBroken() ? 1 : 0;
+                newBrokenCount += eggs[selected].isBroken() ? 1 : 0;
+                solve(selected + 1, newBrokenCount);
+                eggs[i].restore(eggs[selected].getWeight());
+                eggs[selected].restore(eggs[i].getWeight());
+            } else {
+                solve(selected + 1, brokenCount);
+            }
+        }
+    }
+}
