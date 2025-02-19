dc5abea2306b070b8b321a91f47c1fbc6b0d2b81
juyoungha <htj3460@gmail.com>
2025-02-19 10:21:31 +0900

    Refactor[BOJ - Q.9489] : 솔루션 코드 성능개선

--- Tree/BOJ_9489.java
+++ Tree/BOJ_9489.java
@@ -1,14 +1,15 @@
 // BOJ - 9489
 // Problem Sheet - https://www.acmicpc.net/problem/9489
 
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
 
     private static int n, k;
+    private static int targetIndex;
     private static int[] seq;
-    private static final int[] parents = new int[1_000_001];
+    private static int[] parents;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,11 +21,15 @@ public class Main {
             k = Integer.parseInt(row[1]);
             if (n == 0 && k == 0) break;
             seq = new int[n];
+            parents = new int[n];
+            Arrays.fill(parents, -1);
             row = br.readLine().split(" ");
             for (int i=0 ; i<n ; i++) {
                 seq[i] = Integer.parseInt(row[i]);
+                if (seq[i] == k) {
+                    targetIndex = i;
+                }
             }
-            Arrays.fill(parents, 0);
             sb.append(solve()).append("\n");
         }
 
@@ -40,16 +45,16 @@ public class Main {
                 leafIndex++;
                 if (leafIndex >= n) break;
             }
-            parents[seq[i]] = seq[leafIndex];
+            parents[i] = leafIndex;
             prev = seq[i];
         }
 
         int count = 0;
         for (int i=1 ; i<parents.length ; i++) {
-            if (i != k
-                    && parents[parents[i]] > 0
-                    && parents[i] != parents[k]
-                    && parents[parents[i]] == parents[parents[k]]) {
+            if (i != targetIndex
+                    && parents[parents[i]] >= 0
+                    && parents[i] != parents[targetIndex]
+                    && parents[parents[i]] == parents[parents[targetIndex]]) {
                 count++;
             }
         }
