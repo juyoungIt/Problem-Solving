20f82c94930e8b203c079fe0f5fbb9a5db7b083f
juyoungha <htj3460@gmail.com>
2025-02-27 13:30:40 +0900

    Solution[BOJ - Q.2667] : use dfs (recursive)

---
+++ DFS/BOJ_2667_recursive.java
@@ -0,0 +1,83 @@
+// BOJ - 2667
+// Problem Sheet - https://www.acmicpc.net/problem/2667
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Point {
+        private final int x;
+        private final int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() { return this.x; }
+        public int getY() { return this.y; }
+    }
+
+    private static int N;
+    private static int[][] map;
+    private static final List<Point> points = new ArrayList<>();
+    private static final int[] xi = { -1, 1, 0, 0 };
+    private static final int[] yi = { 0, 0, -1, 1 };
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        for (int i=0 ; i<N ; i++) {
+            String[] row = br.readLine().split("");
+            for (int j=0 ; j<N ; j++) {
+                map[i][j] = Integer.parseInt(row[j]);
+                if (map[i][j] == 1) {
+                    points.add(new Point(j, i));
+                }
+            }
+        }
+        br.close();
+    }
+
+    private static String solve() {
+        List<Integer> sizes = new ArrayList<>();
+        for (Point point : points) {
+            int size = dfs(point.getX(), point.getY());
+            if (size > 0) {
+                sizes.add(size);
+            }
+        }
+        Collections.sort(sizes);
+        StringBuilder sb = new StringBuilder();
+        sb.append(sizes.size()).append("\n");
+        for (int size : sizes) {
+            sb.append(size).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static int dfs(int sx, int sy) {
+        if (map[sy][sx] == 2) return 0;
+        int size = 0;
+        map[sy][sx] = 2;
+        size++;
+        for (int i=0 ; i<4 ; i++) {
+            int nx = sx + xi[i];
+            int ny = sy + yi[i];
+            if (isNotValid(nx, ny) || map[ny][nx] == 2 || map[ny][nx] == 0) continue;
+            size += dfs(nx, ny);
+        }
+        return size;
+    }
+
+    private static boolean isNotValid(int x, int y) {
+        return !(x>=0 && y>=0 && x<N && y<N);
+    }
+}
