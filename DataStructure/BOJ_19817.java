2dc588c11d114be26338e5ebbce89df1823bc7ae
juyoungha <htj3460@gmail.com>
2025-02-28 19:28:40 +0900

    Refactor[BOJ - Q.19817] : 솔루션 코드 성능개선

--- DataStructure/BOJ_19817.java
+++ DataStructure/BOJ_19817.java
@@ -1,13 +1,14 @@
 // BOJ - 19817
 // Problem Sheet - https://www.acmicpc.net/problem/19817
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
 
     private static int n;
-    private static List<Integer>[] salaries;
+    private static int[] maxSalaries;
+    private static int[] sizes;
+    private static int topSalary = 0;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -17,25 +18,23 @@ public class Main {
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        salaries = new ArrayList[n];
+        maxSalaries = new int[n];
+        sizes = new int[n];
         for (int i=0 ; i<n ; i++) {
-            salaries[i] = new ArrayList<>();
             String[] row = br.readLine().split(" ");
-            int m = Integer.parseInt(row[0]);
-            for (int j=0 ; j<m ; j++) {
-                salaries[i].add(Integer.parseInt(row[j + 1]));
+            sizes[i] = Integer.parseInt(row[0]);
+            for (int j=1 ; j<=sizes[i] ; j++) {
+                maxSalaries[i] = Math.max(maxSalaries[i], Integer.parseInt(row[j]));
+                topSalary = Math.max(topSalary, maxSalaries[i]);
             }
-            salaries[i].sort(Collections.reverseOrder());
         }
         br.close();
     }
 
     private static long solve() {
         long totalIncrements = 0;
-        Arrays.sort(salaries, (s1, s2) -> s2.get(0) - s1.get(0));
-        int maxSalary = salaries[0].get(0);
-        for (int i=1 ; i<n ; i++) {
-            totalIncrements += (long) (maxSalary - salaries[i].get(0)) * salaries[i].size();
+        for (int i=0 ; i<n ; i++) {
+            totalIncrements += (long) (topSalary - maxSalaries[i]) * sizes[i];
         }
         return totalIncrements;
     }
