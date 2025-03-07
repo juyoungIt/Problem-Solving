0927b8d8b93f8132c859658ca9e02681d887be41
juyoungha <htj3460@gmail.com>
2025-03-07 19:21:03 +0900

    Refactor[BOJ - Q.2206] : 솔루션 코드 성능개선

--- BFS/BOJ_2206.java
+++ BFS/BOJ_2206.java
@@ -4,95 +4,84 @@
 import java.util.*;
 import java.io.*;
 
-class Location {
-    private final int x; // x좌표
-    private final int y; // y좌표
-    private final int depth; // 탐색의 깊이
-    private final int breakWall; // 벽을 부순 적이 있는 가
-
-    public Location(int x, int y, int depth, int breakWall) {
-        this.x = x;
-        this.y = y;
-        this.depth = depth;
-        this.breakWall = breakWall;
-    }
-
-    public int getX() { return this.x; }
-    public int getY() { return this.y; }
-    public int getDepth() { return this.depth; }
-    public int getBreakWall() { return this.breakWall; }
-}
-
 public class Main {
-    private static int n; // 지도 세로
-    private static int m; // 지도 가로
-    private static int[][] map; // 지도정보
-    private static boolean[][][] visit; // 방문정보 (0: 벽을 부순 적 없음, 1: 벽을 부순적 있음)
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        n = Integer.parseInt(st.nextToken()); // 지도 세로
-        m = Integer.parseInt(st.nextToken()); // 지도 가로
-        map = new int[n][m]; // 지도 정보
-        visit = new boolean[2][n][m]; // 방문정보 (0: 벽을 부순 적 없음, 1: 벽을 부순적 있음)
+    static class Point {
+        private final int x;
+        private final int y;
+        private final int useBroker;
+        private final int dist;
 
-        // 입력 내용을 바탕으로 지도의 정보를 구성함
-        for(int i=0 ; i<n ; i++) {
-            String tmp = bf.readLine();
-            for(int j=0 ; j<m ; j++)
-                map[i][j] = Integer.parseInt(tmp.charAt(j)+"");
+        public Point(int x, int y, int useBroker, int dist) {
+            this.x = x;
+            this.y = y;
+            this.useBroker = useBroker;
+            this.dist = dist;
         }
 
-        // BFS를 수행하며 최적을 솔루션을 구함
-        System.out.println(bfs());
-
-        bf.close();
-        System.exit(0);
+        public int getX() { return this.x; }
+        public int getY() { return this.y; }
+        public int isBrokerUsed() { return this.useBroker; }
+        public int getDist() { return this.dist; }
     }
 
-    static int bfs() {
-        Queue<Location> queue = new LinkedList<>();
-        int[] xi = {-1, 1, 0, 0}; // x-increment
-        int[] yi = {0, 0, -1, 1}; // y-increment
-        int count = -1; // 목표 지점에 도착하기 위해 필요한 이동 수
+    private static int N, M;
+    private static boolean[][] map;
+    private static boolean[][][] visitedTimes;
 
-        // 시작지점 위치를 주입함
-        visit[0][0][0] = true;
-        queue.add(new Location(0, 0, 1, 0));
-        while(!queue.isEmpty()) {
-            int curX = queue.peek().getX();
-            int curY = queue.peek().getY();
-            int curDepth = queue.peek().getDepth();
-            int curBreakWall = queue.peek().getBreakWall();
+    private static final int[] xi = { -1, 1, 0, 0 };
+    private static final int[] yi = { 0, 0, -1, 1 };
 
-            // 목표 지점에 도착한 경우
-            if(curX == m-1 && curY == n-1) {
-                count = curDepth;
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        map = new boolean[N][M];
+        visitedTimes = new boolean[N][M][2];
+        for (int i=0 ; i<N ; i++) {
+            String row = br.readLine();
+            for (int j=0 ; j<M ; j++) {
+                map[i][j] = row.charAt(j) == '0';
+            }
+        }
+        br.close();
+    }
+
+    private static int solve() {
+        int dist = -1;
+        Queue<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(0, 0, 0, 1));
+        visitedTimes[0][0][0] = true;
+        while (!queue.isEmpty()) {
+            Point cur = queue.poll();
+            if (cur.getX() == M-1 && cur.getY() == N-1) {
+                dist = cur.getDist();
                 break;
             }
-
-            for(int i=0 ; i<4 ; i++) {
-                int tx = curX + xi[i];
-                int ty = curY + yi[i];
-                // 사용할 수 있는 유효한 좌표인가?
-                if(validation(tx, ty)) {
-                    // 방문 가능한 빈 공간을 만난 경우
-                    if(map[ty][tx] == 0 && !visit[curBreakWall][ty][tx]) {
-                        visit[curBreakWall][ty][tx] = true;
-                        queue.add(new Location(tx, ty, curDepth+1, curBreakWall));
-                    }
-                    // 방문이 불가능한 벽을 만난 경우
-                    else if(curBreakWall == 0 && map[ty][tx] == 1 && !visit[curBreakWall][ty][tx]) {
-                        visit[curBreakWall][ty][tx] = true;
-                        queue.add(new Location(tx, ty, curDepth+1, 1));
-                    }
+            for (int i=0 ; i<4 ; i++) {
+                int nx = cur.getX() + xi[i];
+                int ny = cur.getY() + yi[i];
+                if (isNotValid(nx, ny)) continue;
+                if (visitedTimes[ny][nx][cur.isBrokerUsed()]) continue;
+                if (map[ny][nx]) {
+                    queue.add(new Point(nx, ny, cur.isBrokerUsed(), cur.getDist() + 1));
+                    visitedTimes[ny][nx][cur.isBrokerUsed()] = true;
+                } else if (cur.isBrokerUsed() == 0) {
+                    queue.add(new Point(nx, ny, 1, cur.getDist() + 1));
+                    visitedTimes[ny][nx][1] = true;
                 }
             }
-            queue.poll();
         }
-        return count;
+        return dist;
     }
 
-    static boolean validation(int x, int y) { return (x>=0 && y>=0 && x<m && y<n); }
+    private static boolean isNotValid(int x, int y) {
+        return !(x>=0 && y>=0 && x<M && y<N);
+    }
 }
