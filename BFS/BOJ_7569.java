e0ddc2769b0bd5d8e42198742f6f16fa732014a9
juyoungha <htj3460@gmail.com>
2025-03-09 09:17:21 +0900

    Refactor[BOJ - Q.7569] : 솔루션 코드 성능개선

--- BFS/BOJ_7569.java
+++ BFS/BOJ_7569.java
@@ -4,114 +4,86 @@
 import java.util.*;
 import java.io.*;
 
-class Location {
-    private final int x;
-    private final int y;
-    private final int z;
-    private final int depth;
-
-    public Location(int x, int y, int z, int depth) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.depth = depth;
-    }
-
-    public int getX() { return this.x; }
-    public int getY() { return this.y; }
-    public int getZ() { return this.z; }
-    public int getDepth() { return this.depth; }
-}
-
 public class Main {
-    private static Queue<Location> queue; // bfs에 사용하는 queue
-    private static int m; // 상자 가로 칸
-    private static int n; // 상자 세로 칸
-    private static int h; // 쌓이는 상자 수
-    private static int[][][] map; // 토마토를 놓는 트레이
-    private static boolean[][][] visit; // 방문정보
+
+    static class Point {
+        private final int x;
+        private final int y;
+        private final int z;
+        private final int day;
+
+        public Point(int x, int y, int z, int day) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.day = day;
+        }
+
+        public int getX() { return this.x; }
+        public int getY() { return this.y; }
+        public int getZ() { return this.z; }
+        public int getDay() { return this.day; }
+    }
+
+    private static int M, N, H;
+    private static int[][][] box;
+    private static int unripeCount = 0;
+
+    private static final Queue<Point> queue = new ArrayDeque<>();
+    private static final int[] xi = { -1, 1, 0, 0, 0, 0 };
+    private static final int[] yi = { 0, 0, -1, 1, 0, 0 };
+    private static final int[] zi = { 0, 0, 0, 0, -1, 1 };
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        queue = new LinkedList<>();
-        boolean done = true; // 이미 모든 토마토가 익었는 가?
-        int days = -1;
-
-        // 토마토를 담는 트레이에 대한 기본정보를 구성함
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
-        map = new int[h][n][m];
-        visit = new boolean[h][n][m];
-
-        // 각 층별로 토마토의 정보를 입력
-        for(int i=0 ; i<h ; i++) {
-            for(int j=0 ; j<n ; j++) {
-                st = new StringTokenizer(bf.readLine());
-                for(int k=0 ; k<m ; k++) {
-                    map[i][j][k] = Integer.parseInt(st.nextToken());
-                    if(map[i][j][k] == 1) {
-                        visit[i][j][k] = true;
-                        queue.add(new Location(k, j, i, 0));
-                    }
-                    if(map[i][j][k] == 0) done = false;
-                }
-            }
-        }
-
-        if(done) days = 0;
-        else days = bfs();
-        System.out.println(days);
-
-        bf.close();
-        System.exit(0);
+        input();
+        System.out.println(solve());
     }
 
-    static int bfs() {
-        int[] xi = {-1, 1, 0, 0, 0, 0}; // x-increment
-        int[] yi = {0, 0, -1, 1, 0, 0}; // y-increment
-        int[] zi = {0, 0, 0, 0, -1, 1}; // z-increment
-        int days = 0;
-
-        while(!queue.isEmpty()) {
-            int curX = queue.peek().getX();
-            int curY = queue.peek().getY();
-            int curZ = queue.peek().getZ();
-            int curDepth = queue.peek().getDepth();
-            if(days < curDepth)
-                days = curDepth;
-            for(int i=0 ; i<6 ; i++) {
-                int tx = curX + xi[i];
-                int ty = curY + yi[i];
-                int tz = curZ + zi[i];
-                if(validation(tx, ty, tz) && map[tz][ty][tx] == 0 && !visit[tz][ty][tx]) {
-                    visit[tz][ty][tx] = true;
-                    map[tz][ty][tx] = 2;
-                    queue.add(new Location(tx, ty, tz, curDepth+1));
-                }
-            }
-            queue.poll();
-        }
-        return (isFinished()) ? days : -1;
-    }
-
-    // 모든 토마토가 익었는 지 검사함
-    static boolean isFinished() {
-        boolean result = true;
-        for(int i=0 ; i<h ; i++) {
-            for(int j=0 ; j<n ; j++) {
-                for(int k=0 ; k<m ; k++) {
-                    if(map[i][j][k] == 0) {
-                        result = false;
-                        break;
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        M = Integer.parseInt(input[0]);
+        N = Integer.parseInt(input[1]);
+        H = Integer.parseInt(input[2]);
+        box = new int[H][N][M];
+        for (int i=0 ; i<H ; i++) {
+            for (int j=0 ; j<N ; j++) {
+                String[] row = br.readLine().split(" ");
+                for (int k=0 ; k<M ; k++) {
+                    box[i][j][k] = Integer.parseInt(row[k]);
+                    if (box[i][j][k] == 0) {
+                        unripeCount++;
+                    } else if (box[i][j][k] == 1) {
+                        queue.add(new Point(k, j, i, 0));
+                        box[i][j][k] = 1;
                     }
                 }
             }
         }
-        return result;
+        br.close();
     }
 
-    static boolean validation(int x, int y, int z) {
-        return (x>=0 && y>=0 && z>=0 && x<m && y<n && z<h);
+    private static int solve() {
+        int day = -1;
+        while (!queue.isEmpty()) {
+            Point cur = queue.poll();
+            if (unripeCount == 0) {
+                day = cur.getDay();
+            }
+            for (int i=0 ; i<6 ; i++) {
+                int nx = cur.getX() + xi[i];
+                int ny = cur.getY() + yi[i];
+                int nz = cur.getZ() + zi[i];
+                if (isOutOfRange(nx, ny, nz) || box[nz][ny][nx] != 0) continue;
+                queue.add(new Point(nx, ny, nz, cur.getDay() + 1));
+                box[nz][ny][nx] = 1;
+                unripeCount--;
+            }
+        }
+        return day;
+    }
+
+    private static boolean isOutOfRange(int x, int y, int z) {
+        return !(x>=0 && y>=0 && z>=0 && x<M && y<N && z<H);
     }
 }
