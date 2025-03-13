9b1505873fa7393029ea552d01a6ed94f3a19d99
juyoungha <htj3460@gmail.com>
2025-03-13 20:10:21 +0900

    Refactor[BOJ - Q.2748] : 솔루션 코드 성능개선

--- DP/BOJ_2748.java
+++ DP/BOJ_2748.java
@@ -1,23 +1,30 @@
 // BOJ - 2748
 // Problem Sheet - https://www.acmicpc.net/problem/2748
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    private static int n;
+    private static long[] fibo;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine()); // 원하는 차수
-        long[] fSequence = new long[n+1]; // 피보나치 수의 수열을 저장함
-        fSequence[0] = 0; // 기본 정의값 주입
-        fSequence[1] = 1; // 기본 정의값 주입
+        input();
+        System.out.println(solve());
+    }
 
-        for(int i=2 ; i<=n ; i++)
-            fSequence[i] = fSequence[i-2] + fSequence[i-1];
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        fibo = new long[n + 1];
+        br.close();
+    }
 
-        System.out.println(fSequence[n]);
-
-        bf.close();
-        System.exit(0);
+    private static long solve() {
+        fibo[1] = 1;
+        for (int i=2 ; i<=n ; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        return fibo[n];
     }
 }
