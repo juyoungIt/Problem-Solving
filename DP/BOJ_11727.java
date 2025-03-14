03ad47cab54a29d22855cb83b35bbf476c95aa6e
juyoungha <htj3460@gmail.com>
2025-03-14 22:46:25 +0900

    Refactor[BOJ - Q.11727] : use top-down approach, 솔루션 코드 성능개선

--- DP/BOJ_11727.java
+++ DP/BOJ_11727.java
@@ -1,30 +1,27 @@
 // BOJ - 11727
 // Problem Sheet - https://www.acmicpc.net/problem/11727
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    private static int[] storage;
+    private static final int DIVIDER = 10_007;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine()); // 목표하는 수
-        long[] dp = new long[n+1]; // dp를 위해 사용하는 cache
-
-        // 기존에 정의된 기본값을 주입함
-        dp[1] = 1;
-        if(n >= 2) dp[2] = 3;
-        System.out.println(getCount(dp, n));
-
-        bf.close();
-        System.exit(0);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        storage = new int[n + 1];
+        System.out.println(solve(n));
+        br.close();
     }
 
-    public static long getCount(long[] dp, int n) {
-        if(n == 1) return dp[1];
-        else if(n == 2) return dp[2];
-        else {
-            if(dp[n] != 0) return dp[n]%10007;
-            else return dp[n] = (getCount(dp,n-1) + getCount(dp,n-2) * 2)%10007;
+    private static int solve(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 3;
+        if (storage[n] == 0) {
+            storage[n] = (solve(n - 2) * 2 + solve(n - 1)) % DIVIDER;
         }
+        return storage[n];
     }
 }
