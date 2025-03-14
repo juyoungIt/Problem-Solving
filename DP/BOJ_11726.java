2ddd90d8ba837aaa8955f46b3d9768ccf43c1924
juyoungha <htj3460@gmail.com>
2025-03-14 21:32:43 +0900

    Refactor[BOJ - Q.11726] : use top-down approach, 솔루션 코드 성능개선

--- DP/BOJ_11726.java
+++ DP/BOJ_11726.java
@@ -1,23 +1,27 @@
 // BOJ - 11726
 // Problem Sheet - https://www.acmicpc.net/problem/11726
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    private static int[] storage;
+    private static final int DIVIDER = 10_007;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine()); // 사각형의 가로 사이즈
-        long[] cache = new long[1000+1]; // dp를 위해 사용할 cache
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        storage = new int[n + 1];
+        System.out.println(solve(n));
+        br.close();
+    }
 
-        cache[1] = 1;
-        cache[2] = 2;
-        for(int i=3 ; i<=n ; i++)
-            cache[i] = (cache[i-2] + cache[i-1])%10007;
-
-        System.out.println(cache[n]%10007);
-
-        bf.close();
-        System.exit(0);
+    private static int solve(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (storage[n] == 0) {
+            storage[n] = (solve(n - 2) + solve(n - 1)) % DIVIDER;
+        }
+        return storage[n];
     }
 }
