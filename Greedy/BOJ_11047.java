6597956fcdb7d31026f0cfe5e3838189c2d6948c
juyoungha <htj3460@gmail.com>
2025-03-15 10:34:59 +0900

    Refactor[BOJ - Q.11047] : 솔루션 코드 성능개선

--- Greedy/BOJ_11047.java
+++ Greedy/BOJ_11047.java
@@ -1,31 +1,36 @@
 // BOJ - 11047
 // Problem Sheet - https://www.acmicpc.net/problem/11047
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner key = new Scanner(System.in);
-		int n = key.nextInt(); // the number of coins
-		int k = key.nextInt(); // values
-		int[] values = new int[n]; // the unit of coins
-		for(int i=0 ; i<n ; i++)
-			values[i] = key.nextInt();
-		int counts = 0; // the minimum number of coins
-		int tmp = 0;    // cache for the calculation
-		
-		// find the optimal solutions
-		for(int i=n-1 ; i>=0 ; i--) {
-			if(tmp == k) break;
-			if(values[i] <= k-tmp) {
-				counts += (k-tmp)/values[i];
-				tmp += values[i] * ((k-tmp)/values[i]);
-			}
-		}
 
-		System.out.println(counts);
+    private static int N, K;
+    private static int[] coins;
 
-		key.close();
-		System.exit(0);
-	}
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        K = Integer.parseInt(input[1]);
+        coins = new int[N];
+        for (int i=0 ; i<N ; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+    }
+
+    private static int solve() {
+        int coinCount = 0;
+        for (int i=N-1 ; i>=0 ; i--) {
+            coinCount += K / coins[i];
+            K %= coins[i];
+        }
+        return coinCount;
+    }
 }
