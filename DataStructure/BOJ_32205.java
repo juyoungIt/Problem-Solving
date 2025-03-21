fc92f9ab914e648a83e248c89b2d4a6a6e6e5ef4
juyoungha <htj3460@gmail.com>
2025-03-21 11:10:37 +0900

    Refactor[BOJ - Q.32205] : 카운트 방식을 변경하여 성능개선 (1584ms -> 552ms)

--- DataStructure/BOJ_32205.java
+++ DataStructure/BOJ_32205.java
@@ -1,39 +1,28 @@
 // BOJ - 32205
 // Problem Sheet - https://www.acmicpc.net/problem/32205
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    private static Map<Integer, Integer> triangles;
-
     public static void main(String[] args) throws IOException {
-        input();
-        System.out.println(solve() ? 1 : 0);
-    }
-
-    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] counts = new int[1_000_001];
         int N = Integer.parseInt(br.readLine());
-        triangles = new HashMap<>();
+        boolean isAvailable = false;
         while (N-- > 0) {
             String[] rect = br.readLine().split(" ");
             int a = Integer.parseInt(rect[0]);
             int b = Integer.parseInt(rect[1]);
             int c = Integer.parseInt(rect[2]);
-            triangles.put(a, triangles.getOrDefault(a, 0) + 1);
-            triangles.put(b, triangles.getOrDefault(b, 0) + 1);
-            triangles.put(c, triangles.getOrDefault(c, 0) + 1);
+            counts[a]++;
+            counts[b]++;
+            counts[c]++;
+            if (counts[a] >= 2 || counts[b] >= 2 || counts[c] >= 2) {
+                isAvailable = true;
+                break;
+            }
         }
+        System.out.println(isAvailable ? 1 : 0);
         br.close();
     }
-
-    private static boolean solve() {
-        for (int count : triangles.values()) {
-            if (count >= 2) return true;
-        }
-        return false;
-    }
-
 }
