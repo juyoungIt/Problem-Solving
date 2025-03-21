db3eb2b17386b0ef32b24d18c10acef714ec2aea
juyoungha <htj3460@gmail.com>
2025-03-21 20:54:07 +0900

    Refactor[BOJ - Q.2294] : DP 갱신 시 금액중심 -> 동전중심 순회로 개선하여 더 많은 다양한 케이스에 유리한 형태로 개선

--- DP/BOJ_2294_bottom_up.java
+++ DP/BOJ_2294_bottom_up.java
@@ -7,9 +7,10 @@ import java.io.*;
 public class Main {
 
     private static int n, k;
-    private static int[] c;
+    private static int[] coin;
     private static int[] dp;
-    private static final int IMPOSSIBLE = 10_001;
+
+    private static final int NOT_EXIST = 10_001;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -21,24 +22,23 @@ public class Main {
         String[] row = br.readLine().split(" ");
         n = Integer.parseInt(row[0]);
         k = Integer.parseInt(row[1]);
-        dp = new int[k + 1];
-        Arrays.fill(dp, IMPOSSIBLE);
-        c = new int[n];
+        coin = new int[n];
         for (int i=0 ; i<n ; i++) {
-            c[i] = Integer.parseInt(br.readLine());
-            if (c[i] <= k) dp[c[i]] = 1;
+            coin[i] = Integer.parseInt(br.readLine());
         }
+        dp = new int[k + 1];
+        Arrays.fill(dp, NOT_EXIST);
         br.close();
     }
 
     private static int solve() {
-        for (int i=0 ; i<=k ; i++) {
-            if (dp[i] == IMPOSSIBLE) continue;
-            for (int j=0 ; j<n ; j++) {
-                if (i + c[j] > k) continue;
-                dp[i + c[j]] = Math.min(dp[i + c[j]], dp[i] + 1);
+        dp[0] = 0;
+        for (int i=0 ; i<n ; i++) {
+            for (int j=0 ; j + coin[i]<=k ; j++) {
+                if (dp[j] == NOT_EXIST) continue;
+                dp[j + coin[i]] = Math.min(dp[j + coin[i]], dp[j] + 1);
             }
         }
-        return dp[k] == IMPOSSIBLE ? -1 : dp[k];
+        return dp[k] == NOT_EXIST ? -1 : dp[k];
     }
 }
