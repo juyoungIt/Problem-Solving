8d07b93b1e403ead4bb292b2d534dc27b8e5a110
juyoungha <htj3460@gmail.com>
2025-03-25 09:48:37 +0900

    Refactor[BOJ - Q.7847] : 솔루션 코드 성능개선 (HashMap -> TreeMap)

--- DataStructure/BOJ_7847.java
+++ DataStructure/BOJ_7847.java
@@ -3,59 +3,38 @@
 
 import java.util.*;
 import java.io.*;
-import java.util.stream.Collectors;
 
 public class Main {
-
-    private static int N;
-    private static int[][] sales;
-    private static Set<Integer> itemSet, storeSet;
-    private static Map<String, Long> storage;
-
     public static void main(String[] args) throws IOException {
-        input();
-        System.out.println(solve());
-    }
-
-    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        sales = new int[N][3];
-        itemSet = new HashSet<>();
-        storeSet = new HashSet<>();
-        for (int i=0 ; i<N ; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            sales[i][0] = Integer.parseInt(st.nextToken());
-            sales[i][1] = Integer.parseInt(st.nextToken());
-            sales[i][2] = Integer.parseInt(st.nextToken());
-            itemSet.add(sales[i][0]);
-            storeSet.add(sales[i][1]);
-        }
-        storage = new HashMap<>();
-        br.close();
-    }
+        StringBuilder report = new StringBuilder();
+        StringTokenizer st;
 
-    private static String solve() {
-        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer, Map<Integer, Long>> storage = new TreeMap<>();
+        Set<Integer> itemSet = new TreeSet<>();
         for (int i=0 ; i<N ; i++) {
-            String key = sales[i][1] + "-" + sales[i][0];
-            int count = sales[i][2];
-            storage.put(key, storage.getOrDefault(key, 0L) + count);
+            st = new StringTokenizer(br.readLine());
+            int item = Integer.parseInt(st.nextToken());
+            int store = Integer.parseInt(st.nextToken());
+            int count = Integer.parseInt(st.nextToken());
+            if (!storage.containsKey(store)) storage.put(store, new HashMap<>());
+            storage.get(store).put(item, storage.get(store).getOrDefault(item, 0L) + count);
+            itemSet.add(item);
         }
 
-        List<Integer> items = itemSet.stream().sorted().collect(Collectors.toList());
-        List<Integer> stores = storeSet.stream().sorted().collect(Collectors.toList());
-        sb.append("-1 ");
-        items.forEach(i -> sb.append(i).append(" "));
-        sb.append("\n");
-        for (int store : stores) {
-            sb.append(store).append(" ");
-            for (int item : items) {
-                String key = store + "-" + item;
-                sb.append(storage.getOrDefault(key, 0L)).append(" ");
+        report.append("-1 ");
+        for (int item : itemSet) report.append(item).append(" ");
+        report.append("\n");
+        for (int store : storage.keySet()) {
+            report.append(store).append(" ");
+            for (int item : itemSet) {
+                report.append(storage.get(store).getOrDefault(item, 0L)).append(" ");
             }
-            sb.append("\n");
+            report.append("\n");
         }
-        return sb.toString();
+
+        System.out.println(report);
+        br.close();
     }
 }
