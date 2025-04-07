a12e34fae099b05aa6c7b99525b38ca93ac4c1a0
juyoungha <htj3460@gmail.com>
2025-04-07 11:19:27 +0900

    Refactor[BOJ - Q.2096] : 솔루션 코드 메모리 사용량 최적화

--- DP/BOJ_2096.java
+++ DP/BOJ_2096.java
@@ -6,51 +6,30 @@ import java.io.*;
 
 public class Main {
 
-	private static int N;
-	private static int[][] b;
-	private static int[] dmx, dmn;
-	private static int[] tmx, tmn; // 임시저장 용도
+	private static final int IMPOSSIBLE = 1_000_000;
 
 	public static void main(String[] args) throws IOException {
-		input();
-		System.out.println(solve());
-	}
-
-	private static void input() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		b = new int[N][5];
-		for (int i=0 ; i<N ; i++) {
+		int[] dmx = new int[5];
+		int[] dmn = new int[5];
+		dmn[0] = dmn[4] = IMPOSSIBLE;
+		int[] tmx = new int[5]; 
+		int[] tmn = new int[5];
+		tmn[0] = tmn[4] = IMPOSSIBLE;
+		int n = Integer.parseInt(br.readLine());
+		for (int i=0 ; i<n ; i++) {
 			String[] row = br.readLine().split(" ");
 			for (int j=1 ; j<=3 ; j++) {
-				b[i][j] = Integer.parseInt(row[j - 1]);
+				int value = Integer.parseInt(row[j - 1]);
+				tmx[j] = value + Math.max(dmx[j - 1], Math.max(dmx[j], dmx[j + 1]));
+				tmn[j] = value + Math.min(dmn[j - 1], Math.min(dmn[j], dmn[j + 1]));
 			}
-		}
-		dmx = new int[5];
-		dmn = new int[5];
-		tmx = new int[5];
-		tmn = new int[5];
-		Arrays.fill(dmn, 900_001);
-		br.close();
-	}
-
-	private static String solve() {
-		for (int i=1 ; i<=3 ; i++) {
-			dmx[i] = dmn[i] = b[0][i];
-		}
-		for (int i=1 ; i<N ; i++) {
-			for (int j=1 ; j<=3 ; j++) {
-				tmx[j] = b[i][j] + Math.max(dmx[j - 1], Math.max(dmx[j], dmx[j + 1]));
-				tmn[j] = b[i][j] + Math.min(dmn[j - 1], Math.min(dmn[j], dmn[j + 1]));
-			}
-			for (int j=1 ; j<=3 ; j++) {
-				dmx[j] = tmx[j];
-				dmn[j] = tmn[j];
-			}
+			System.arraycopy(tmx, 0, dmx, 0, 5);
+			System.arraycopy(tmn, 0, dmn, 0, 5);
 		}
 		int max = Math.max(dmx[1], Math.max(dmx[2], dmx[3]));
 		int min = Math.min(dmn[1], Math.min(dmn[2], dmn[3]));
-		return max + " " + min;
+		System.out.printf("%d %d\n", max, min);
+		br.close();
 	}
-
 }
