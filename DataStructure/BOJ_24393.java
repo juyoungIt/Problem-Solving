e6efa519daafabac2923421e3633be56a99a523c
juyoungha <htj3460@gmail.com>
2025-05-09 16:00:59 +0900

    Refactor[BOJ - Q.24393] : 자료구조 개선을 통한 성능개선

--- DataStructure/BOJ_24393.java
+++ DataStructure/BOJ_24393.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class Main {
 
     private static int n;
-    private static List<List<Integer>> a;
+    private static int[][] a;
     private static final int SIZE = 27;
     private static final int LEFT_SIZE = 13, RIGHT_SIZE = 14;
 
@@ -19,12 +19,11 @@ public class Main {
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        a = new ArrayList<>(n);
+        a = new int[n][];
         for (int i=0 ; i<n ; i++) {
-            a.add(new LinkedList<>());
-            Arrays.stream(br.readLine().split(" "))
+            a[i] = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
-                    .forEach(a.get(i)::add);
+                    .toArray();
         }
         br.close();
     }
@@ -43,27 +42,28 @@ public class Main {
             for (int j=0 ; j<LEFT_SIZE ; j++) left.add(deque.pollFirst());
             for (int j=0 ; j<RIGHT_SIZE ; j++) right.add(deque.pollFirst());
             // 2-2. right top 의 Ai1 장을 덱의 상단에 배치
-            for (int j=0 ; j<a.get(i).get(0) ; j++) {
+            for (int j=0 ; j<a[i][0] ; j++) {
                 deque.addLast(right.pollFirst());
             }
             // 2-3. left, right 를 번갈아 가면서 덱에 카드를 배치
-            for (int j=1 ; j<a.get(i).size() ; j++) {
+            for (int j=1 ; j<a[i].length ; j++) {
                 if (j % 2 == 0) {
-                    for (int k=0 ; k<a.get(i).get(j) ; k++) {
+                    for (int k=0 ; k<a[i][j] ; k++) {
                         deque.addLast(right.pollFirst());
                     }
                 } else {
-                    for (int k=0 ; k<a.get(i).get(j) ; k++) {
+                    for (int k=0 ; k<a[i][j] ; k++) {
                         deque.addLast(left.pollFirst());
                     }
                 }
             }
         }
-        int jokerIndex;
-        for (jokerIndex=0 ; jokerIndex<SIZE ; jokerIndex++) {
-            if (!deque.isEmpty() && deque.pollFirst() == 1) break;
+        int jokerIndex = 1;
+        for (int card : deque) {
+            if (card == 1) break;
+            jokerIndex++;
         }
-        return jokerIndex + 1;
+        return jokerIndex;
     }
 
 }
