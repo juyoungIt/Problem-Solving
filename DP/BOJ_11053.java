4155df44d40187a2e3556846f1cb887d913a1612
juyoungha <htj3460@gmail.com>
2025-05-17 22:56:29 +0900

    Refactor[BOJ - Q.11053] : 솔루션 코드 개선

--- DP/BOJ_11053.java
+++ DP/BOJ_11053.java
@@ -1,30 +1,43 @@
 // BOJ - 11053
 // Problem Sheet - https://www.acmicpc.net/problem/11053
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    private static int n;
+    private static int[] a;
+    private static int[] dp;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine()); // 수열의 크기
-        int[] sequence = new int[n]; // 수열의 정보
-        int[] dp = new int[n]; // 수열의 i번째 원소가 포함될 경우의 최대 증가수열의 길이
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        for(int i=0 ; i<n ; i++)
-            sequence[i] = Integer.parseInt(st.nextToken());
-
-        for(int i=0 ; i<n ; i++) {
-            dp[i] = 1;
-            for(int j=0 ; j<i ; j++)
-                if(sequence[j] < sequence[i])
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-        }
-
-        Arrays.sort(dp);
-        System.out.println(dp[n-1]);
-
-        bf.close();
-        System.exit(0);
+        input();
+        System.out.println(solve());
     }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        a = new int[n];
+        String[] row = br.readLine().split(" ");
+        for (int i=0 ; i<n ; i++) {
+            a[i] = Integer.parseInt(row[i]);
+        }
+        dp = new int[n];
+        br.close();
+    }
+
+    private static int solve() {
+        int maxLen = 1;
+        for (int i=0 ; i<n ; i++) {
+            dp[i] = 1;
+            for (int j=0 ; j<i ; j++) {
+                if (a[j] < a[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    maxLen = Math.max(maxLen, dp[i]);
+                }
+            }
+        }
+        return maxLen;
+    }
+
 }
