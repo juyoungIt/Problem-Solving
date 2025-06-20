2eaed3135bb096dbd2eb6f95b96f1a7fb2d66abf
juyoungha <htj3460@gmail.com>
2025-06-20 15:40:10 +0900

    Refactor[BOJ - Q.15988] : 솔루션 코드 개선

--- DP/BOJ_15988.java
+++ DP/BOJ_15988.java
@@ -4,6 +4,9 @@
 import java.io.*;
 
 public class Main {
+
+    private static final int DIVIDER = 1_000_000_009;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -12,19 +15,17 @@ public class Main {
         dp[1] = 1L;
         dp[2] = 2L;
         dp[3] = 4L;
-        for(int i=4 ; i<1_000_001 ; i++) {
-            dp[i] = (dp[i-3] + dp[i-2] + dp[i-1]) % 1_000_000_009;
+        for (int i=4 ; i<=1_000_000 ; i++) {
+            dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % DIVIDER;
         }
 
         int t = Integer.parseInt(br.readLine());
-        for(int i=0 ; i<t ; i++) {
+        while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             sb.append(dp[n]).append("\n");
         }
 
         System.out.println(sb);
-
         br.close();
-        System.exit(0);
     }
 }
