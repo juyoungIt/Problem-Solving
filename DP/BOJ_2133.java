7152f4b29e5186660fc03bac2df399a0ef5a5bdd
juyoungha <htj3460@gmail.com>
2025-06-21 19:03:06 +0900

    Solution[BOJ - Q.2133]

---
+++ DP/BOJ_2133.java
@@ -0,0 +1,35 @@
+// BOJ - 2133
+// Problem Sheet - https://www.acmicpc.net/problem/2133
+
+import java.io.*;
+
+public class Main {
+
+    private static int n;
+    private static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+        br.close();
+    }
+
+    private static int solve() {
+        if (n % 2 == 1) return 0;
+        dp[0] = 1;
+        for (int i=2 ; i<=n ; i+=2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int j=i-4 ; j>=0 ; j-=2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+        return dp[n];
+    }
+
+}
