a9e99d9300e26161b10ab5476bcd99d45b5bb354
juyoungha <htj3460@gmail.com>
2025-06-21 16:51:49 +0900

    Solution[BOJ - Q.1495]

---
+++ DP/BOJ_1495.java
@@ -0,0 +1,52 @@
+// BOJ - 1495
+// Problem Sheet - https://www.acmicpc.net/problem/1495
+
+import java.io.*;
+
+public class Main {
+
+    private static int n, s, m;
+    private static int[] v;
+    private static boolean[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] row = br.readLine().split(" ");
+        n = Integer.parseInt(row[0]);
+        s = Integer.parseInt(row[1]);
+        m = Integer.parseInt(row[2]);
+        v = new int[n + 1];
+        row = br.readLine().split(" ");
+        for (int i=1 ; i<=n ; i++) {
+            v[i] = Integer.parseInt(row[i - 1]);
+        }
+        dp = new boolean[n + 1][m + 1];
+        br.close();
+    }
+
+    private static int solve() {
+        dp[0][s] = true;
+        for (int i=1 ; i<=n ; i++) {
+            for (int j=0 ; j<=m ; j++) {
+                if (dp[i - 1][j]) {
+                    if (j - v[i] >= 0) dp[i][j - v[i]] = true;
+                    if (j + v[i] <= m) dp[i][j + v[i]] = true;
+                }
+            }
+        }
+        int maxVolume = -1;
+        for (int i=m ; i>=0 ; i--) {
+            if (dp[n][i]) {
+                maxVolume = i;
+                break;
+            }
+        }
+        return maxVolume;
+    }
+
+}
