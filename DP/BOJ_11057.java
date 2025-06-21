0e58cb0079f771e6d39abac8d7136c470897fb53
juyoungha <htj3460@gmail.com>
2025-06-21 17:29:53 +0900

    Refactor[BOJ - Q.11057] : 솔루션 코드 개선

--- DP/BOJ_11057.java
+++ DP/BOJ_11057.java
@@ -1,24 +1,34 @@
 // BOJ - 11057
 // Problem Sheet - https://www.acmicpc.net/problem/11057
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner key = new Scanner(System.in);
-		int length = key.nextInt();
-		int[][] dp = new int[length+1][10]; // for dynamic programming
 
-		for(int i=0 ; i<length+1 ; i++) {
-			for(int j=0 ; j<10 ; j++) {
-				if(i == 0 || j == 0) dp[i][j] = 1;
-				else dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 10007;
-			}
-		}
+    private static int n;
+    private static int[][] dp;
+    private static final int DIVIDER = 10_007;
 
-		System.out.println(dp[length][9]);
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
 
-		key.close();
-		System.exit(0);
-	}
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1][10];
+        br.close();
+    }
+
+    private static int solve() {
+        for (int i=0 ; i<=n ; i++) {
+            for (int j=0 ; j<10 ; j++) {
+                if(i == 0 || j == 0) dp[i][j] = 1;
+                else dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % DIVIDER;
+            }
+        }
+        return dp[n][9] % DIVIDER;
+    }
+
 }
