a74904bc88bed3b20fbd94d2cb851be764fc485f
juyoungha <htj3460@gmail.com>
2025-07-05 22:54:59 +0900

    Refactor[BOJ - Q.1932] : 솔루션 코드 최적화

--- DP/BOJ_1932.java
+++ DP/BOJ_1932.java
@@ -1,42 +1,41 @@
 // BOJ - 1932
 // Problem Sheet - https://www.acmicpc.net/problem/1932
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
 
-	private static int n;
-	private static int[][] t, dp;
+    private static int n;
+    private static int[][] tri, dp;
 
-	public static void main(String[] args) throws IOException {
-		input();
-		System.out.println(solve());	
-	}
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
 
-	private static void input() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		t = new int[n + 1][n];
-		for (int i=1 ; i<=n ; i++) {
-			String[] row = br.readLine().split(" ");
-			for (int j=0 ; j<row.length ; j++) {
-				t[i][j] = Integer.parseInt(row[j]);
-			}
-		}
-		dp = new int[n + 1][n];
-		br.close();
-	}
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        tri = new int[n + 1][n + 1];
+        for (int i=1 ; i<=n ; i++) {
+            String[] row = br.readLine().split(" ");
+            for (int j=1 ; j<=row.length ; j++) {
+                tri[i][j] = Integer.parseInt(row[j - 1]);
+            }
+        }
+        dp = new int[n + 1][n + 1];
+        br.close();
+    }
 
-	private static int solve() {
-		dp[1][0] = t[1][0];
-		for (int i=0 ; i<n ; i++) {
-			for (int j=0 ; j<i ; j++) {
-				dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][j] + t[i + 1][j]);
-				dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + t[i + 1][j + 1]);
-			}
-		}
-		Arrays.sort(dp[n]);
-		return dp[n][n - 1];
-	}
+    private static int solve() {
+        int max = 0;
+        for (int i=1 ; i<=n ; i++) {
+            for (int j=1 ; j<=i ; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + tri[i][j];
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max;
+    }
+
 }
