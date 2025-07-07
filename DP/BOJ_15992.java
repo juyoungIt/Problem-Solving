f5824cc37447c67960a37bfc689355b43525ba61
juyoungha <htj3460@gmail.com>
2025-07-07 17:42:46 +0900

    Refactor[BOJ - Q.15992] : 불필요한 코드 제거

--- DP/BOJ_15992.java
+++ DP/BOJ_15992.java
@@ -5,21 +5,19 @@ import java.io.*;
 
 public class Main {
 
-    private static final int SIZE = 1_000;
-    private static final int DIVIDER = 1_000_000_009;
+    static final int RANGE = 1_000;
+    static final int DIVIDER = 1_000_000_009;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int[][] dp = new int[SIZE + 1][SIZE + 1];
+        long[][] dp = new long[RANGE + 1][RANGE + 1];
         dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][3] = 1;
         dp[3][2] = 2;
-        for (int i=4 ; i<=SIZE ; i++) {
+        for (int i=4 ; i<=RANGE ; i++) {
             for (int j=1 ; j<=i ; j++) {
-                if (j == 1) dp[i][j] = 0;
-                else if (j == i) dp[i][j] = 1;
-                else dp[i][j] = ((dp[i - 1][j - 1] + dp[i - 2][j - 1]) % DIVIDER + dp[i - 3][j - 1]) % DIVIDER;
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 2][j - 1] + dp[i - 3][j - 1]) % DIVIDER;
             }
         }
 
@@ -30,7 +28,6 @@ public class Main {
             int m = Integer.parseInt(row[1]);
             sb.append(dp[n][m]).append("\n");
         }
-
         System.out.println(sb);
         br.close();
     }
