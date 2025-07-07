c0bde7b6a17edc8f50e67d2ac583baa6083c97d8
juyoungha <htj3460@gmail.com>
2025-07-07 18:03:38 +0900

    Refactor[BOJ - Q.11055] : 솔루션 코드 개선

--- DP/BOJ_11055.java
+++ DP/BOJ_11055.java
@@ -5,41 +5,37 @@ import java.io.*;
 
 public class Main {
 
-    private static int n;
-    private static int[] a;
-    private static int[] dp;
-    private static int maxElement = 0;
+    static int n;
+    static int[] a, dp;
 
     public static void main(String[] args) throws IOException {
         input();
         System.out.println(solve());
     }
 
-    private static void input() throws IOException {
+    static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         a = new int[n];
         String[] row = br.readLine().split(" ");
         for (int i=0 ; i<n ; i++) {
             a[i] = Integer.parseInt(row[i]);
-            maxElement = Math.max(maxElement, a[i]);
         }
         dp = new int[n];
         br.close();
     }
 
-    private static int solve() {
-        int maxSum = maxElement;
+    static int solve() {
+        int maxElementSum = 0;
         for (int i=0 ; i<n ; i++) {
             dp[i] = a[i];
             for (int j=0 ; j<i ; j++) {
-                if (a[j] < a[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + a[i]);
-                    maxSum = Math.max(maxSum, dp[i]);
-                }
+                if (a[i] <= a[j]) continue;
+                dp[i] = Math.max(dp[i], dp[j] + a[i]);
             }
+            maxElementSum = Math.max(maxElementSum, dp[i]);
         }
-        return maxSum;
+        return maxElementSum;
     }
 
 }
