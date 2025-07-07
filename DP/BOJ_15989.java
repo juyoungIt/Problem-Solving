a6644c068a0a4148bdf3de8c98d421dfe89ff64e
juyoungha <htj3460@gmail.com>
2025-07-07 15:24:39 +0900

    Refactor[BOJ - Q.15989] : 2차원->1차원 배열을 사용하는 방식으로 개선

--- DP/BOJ_15989.java
+++ DP/BOJ_15989.java
@@ -5,27 +5,26 @@ import java.io.*;
 
 public class Main {
 
-    private static final int SIZE = 10_000;
+    static final int RANGE = 10_000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int[][] dp = new int[SIZE + 1][4];
-        dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
-        for (int i=4 ; i<=SIZE ; i++) {
-            dp[i][1] = dp[i - 1][1];
-            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
-            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        int[] dp = new int[RANGE + 1];
+        int[] elements = { 1, 2, 3 };
+        dp[0] = 1;
+        for (int element : elements) {
+            for (int i=0 ; i+element<=RANGE ; i++) {
+                dp[i + element] += dp[i];
+            }
         }
 
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            int sum = dp[n][1] + dp[n][2] + dp[n][3];
-            sb.append(sum).append("\n");
+            sb.append(dp[n]).append("\n");
         }
-
         System.out.println(sb);
         br.close();
     }
