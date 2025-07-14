9e7f6d881aa2b6e9f17bbe7f79db16b92f53c599
juyoungha <htj3460@gmail.com>
2025-07-14 11:31:31 +0900

    Solution[BOJ - Q.1927] : PriorityQueue 직접 구현버전 추가

---
+++ DataStructure/BOJ_1927_custom.java
@@ -0,0 +1,67 @@
+// BOJ - 1927
+// Problem Sheet - https://www.acmicpc.net/problem/1927
+
+import java.io.*;
+
+public class Main {
+
+    static class MyPriorityQueue {
+        int[] heap;
+        int size;
+
+        public MyPriorityQueue() {
+            heap = new int[SIZE + 1];
+            size = 0;
+        }
+
+        public void add(int x) {
+            heap[++size] = x;
+            int cur = size;
+            while (cur > 1) {
+                int parent = cur / 2;
+                if (heap[parent] <= heap[cur]) break;
+                swap(parent, cur);
+                cur = parent;
+            }
+        }
+
+        public int pop() {
+            if (size == 0) return 0;
+            int root = heap[1];
+            heap[1] = heap[size--];
+            int cur = 1;
+            while (cur * 2 <= size) {
+                int left = cur * 2;
+                int right = left + 1;
+                int child = (right <= size && heap[left] >= heap[right]) ? right : left;
+                if (heap[child] >= heap[cur]) break;
+                swap(child, cur);
+                cur = child;
+            }
+            return root;
+        }
+
+        private void swap(int idx1, int idx2) {
+            int tmp = heap[idx1];
+            heap[idx1] = heap[idx2];
+            heap[idx2] = tmp;
+        }
+    }
+
+    static final int SIZE = 100_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        MyPriorityQueue pQueue = new MyPriorityQueue();
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) sb.append(pQueue.pop()).append("\n");
+            else pQueue.add(x);
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+}
