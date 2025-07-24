b651283d25a86db62706d8515bd2e7d61c859b6b
juyoungha <htj3460@gmail.com>
2025-07-24 23:40:29 +0900

    Refactor[BOJ - Q.1516] : 솔루션 코드 실행시간, 가독성 개선

--- Sorting/BOJ_1516.java
+++ Sorting/BOJ_1516.java
@@ -5,81 +5,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static class Building {
-
-        private final int number;
-        private int buildTime;
-        private int prevBuildingCnt;
-        private final List<Building> nextBuildings;
-
-        public Building(int number, int buildTime) {
-            this.number = number;
-            this.buildTime = buildTime;
-            this.prevBuildingCnt = 0;
-            this.nextBuildings = new LinkedList<>();
-        }
-
-        public int getNumber() { return this.number; }
-        public int getBuildTime() { return this.buildTime; }
-        public int getPrevBuildCnt() { return this.prevBuildingCnt; }
-        public List<Building> getNextBuildings() { return this.nextBuildings; }
-        public void setBuildTime(int buildTime) { this.buildTime = buildTime; }
-        public void setPrevBuildCnt(int prevBuildCnt) { this.prevBuildingCnt = prevBuildCnt; }
-
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        Building[] buildings = new Building[n + 1];
-        int[] accBuildTime = new int[n + 1];
-        for(int i=1 ; i<=n ; i++) {
-            buildings[i] = new Building(i, 0);
+        int[] bt = new int[n + 1];
+        int[] dp = new int[n + 1];
+        int[] indegree = new int[n + 1];
+        List<Integer>[] al = new ArrayList[n + 1];
+        for (int i=1 ; i<=n ; i++) {
+            al[i] = new ArrayList<>();
         }
-
-        for(int i=1 ; i<=n ; i++) {
-            st = new StringTokenizer(br.readLine());
-            int buildTime = Integer.parseInt(st.nextToken());
-            buildings[i].setBuildTime(buildTime);
-            while(st.hasMoreTokens()) {
-                int prevBuildingNumber = Integer.parseInt(st.nextToken());
-                if(prevBuildingNumber == -1) break;
-                buildings[prevBuildingNumber].getNextBuildings().add(buildings[i]);
-                buildings[i].setPrevBuildCnt(buildings[i].getPrevBuildCnt() + 1);
+        for (int i=1 ; i<=n ; i++) {
+            String[] row = br.readLine().split(" ");
+            bt[i] = Integer.parseInt(row[0]);
+            for (int j=1 ; j<row.length ; j++) {
+                int b = Integer.parseInt(row[j]);
+                if (b == -1) break;
+                al[b].add(i);
+                indegree[i]++;
             }
         }
 
-        Queue<Building> waiting = new LinkedList<>();
-        for(int i=1 ; i<=n ; i++) {
-            if(buildings[i].getPrevBuildCnt() == 0) {
-                waiting.add(buildings[i]);
-                accBuildTime[i] = buildings[i].getBuildTime();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i=1 ; i<=n ; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+                dp[i] = bt[i];
             }
         }
-
-        while(!waiting.isEmpty()) {
-            Building cur = waiting.poll();
-            for(Building building : cur.getNextBuildings()) {
-                accBuildTime[building.getNumber()] = Math.max(
-                        accBuildTime[building.getNumber()],
-                        accBuildTime[cur.getNumber()] + building.getBuildTime());
-                building.setPrevBuildCnt(building.getPrevBuildCnt() - 1);
-                if(building.getPrevBuildCnt() == 0) {
-                    waiting.add(building);
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int b : al[cur]) {
+                dp[b] = Math.max(dp[b], dp[cur] + bt[b]);
+                indegree[b]--;
+                if (indegree[b] == 0) {
+                    queue.add(b);
                 }
             }
         }
 
-        for(int i=1 ; i<=n ; i++) {
-            sb.append(accBuildTime[i]).append("\n");
+        StringBuilder sb = new StringBuilder();
+        for (int i=1 ; i<=n ; i++) {
+            sb.append(dp[i]).append("\n");
         }
         System.out.println(sb);
-
         br.close();
-        System.exit(0);
     }
 }
