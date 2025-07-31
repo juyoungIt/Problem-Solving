4f46e3798103fd90d75f54c05ed9d10fe646c6b3
juyoungha <htj3460@gmail.com>
2025-07-31 16:12:17 +0900

    Solution[BOJ - Q.1916] : use adjacent list and priority queue

---
+++ Dijkstra/BOJ_1916_al.java
@@ -0,0 +1,61 @@
+// BOJ - 1916
+// Problem Sheet - https://www.acmicpc.net/problem/1916
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Route {
+        final int dest;
+        final int cost;
+
+        public Route(int dest, int cost) {
+            this.dest = dest;
+            this.cost = cost;
+        }
+    }
+
+    static final int INF = 100_000_001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[] costs = new int[n + 1];
+        List<Route>[] al = new ArrayList[n + 1];
+        for (int i=1 ; i<=n ; i++) {
+            al[i] = new ArrayList<>();
+        }
+        Arrays.fill(costs, INF);
+        for (int i=0 ; i<m ; i++) {
+            String[] row = br.readLine().split(" ");
+            int dept = Integer.parseInt(row[0]);
+            int dest = Integer.parseInt(row[1]);
+            int cost = Integer.parseInt(row[2]);
+            al[dept].add(new Route(dest, cost));
+        }
+        String[] row = br.readLine().split(" ");
+        int dept = Integer.parseInt(row[0]);
+        int dest = Integer.parseInt(row[1]);
+
+        PriorityQueue<Route> pq = new PriorityQueue<>(Comparator.comparingInt(r -> r.cost));
+        pq.add(new Route(dept, 0));
+        costs[dept] = 0;
+        while (!pq.isEmpty()) {
+            Route cur = pq.poll();
+            if (costs[cur.dest] < cur.cost) continue;
+            for (Route route : al[cur.dest]) {
+                if (costs[route.dest] > costs[cur.dest] + route.cost) {
+                    costs[route.dest] = costs[cur.dest] + route.cost;
+                    pq.add(new Route(route.dest, costs[route.dest]));
+                }
+            }
+        }
+
+        System.out.println(costs[dest]);
+        br.close();
+    }
+
+}
