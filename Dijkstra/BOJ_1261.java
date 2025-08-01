1de8720705b0fd6305f1168286bf09c491364608
juyoungha <htj3460@gmail.com>
2025-08-01 11:00:12 +0900

    Refactor[BOJ - Q.1261] : 솔루션 코드 가독성 개선, 실행시간 단축

--- Dijkstra/BOJ_1261.java
+++ Dijkstra/BOJ_1261.java
@@ -6,79 +6,58 @@ import java.io.*;
 
 public class Main {
 
-    private static int n;
-    private static int m;
-    private static final int[] xi = {-1, 1, 0, 0};
-    private static final int[] yi = {0, 0, -1, 1};
-    private static int[][] map;
-    private static int[][] minCost;
+    static class Status {
+        final int x;
+        final int y;
+        final int count;
 
-    static class Status implements Comparable<Status> {
-        private final int x;
-        private final int y;
-        private final int cost;
-
-        public Status(int x, int y, int cost) {
+        public Status(int x, int y, int count) {
             this.x = x;
             this.y = y;
-            this.cost = cost;
-        }
-
-        public int getX() { return this.x; }
-        public int getY() { return this.y; }
-        public int getCost() { return this.cost; }
-
-        @Override
-        public int compareTo(Status s) {
-            return this.cost - s.getCost();
+            this.count = count;
         }
     }
+
+    static final int[] xi = { -1, 1, 0, 0 };
+    static final int[] yi = { 0, 0, -1, 1 };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        map = new int[n + 1][m + 1];
-        minCost = new int[n + 1][m + 1];
-        for(int i=1 ; i<=n ; i++) {
-            String[] row = br.readLine().split("");
-            for(int j=1 ; j<=m ; j++) {
-                map[i][j] = Integer.parseInt(row[j-1]);
-                minCost[i][j] = n * m;
-            }
-        }
-
-        dijkstra();
-        System.out.println(minCost[n][m]);
-
-        br.close();
-        System.exit(0);
-    }
-
-    private static void dijkstra() {
-        PriorityQueue<Status> pq = new PriorityQueue<>();
-        pq.add(new Status(1, 1, 0));
-        minCost[1][1] = 0;
-        while(!pq.isEmpty()) {
-            Status curStatus = pq.poll();
-            int curX = curStatus.getX();
-            int curY = curStatus.getY();
-            int curCost = curStatus.getCost();
-            for(int i=0 ; i<4 ; i++) {
-                int nextX = curX + xi[i];
-                int nextY = curY + yi[i];
-                if(isValid(nextX, nextY, m, n)
-                        && minCost[nextY][nextX] > minCost[curY][curX] + map[nextY][nextX]) {
-                    minCost[nextY][nextX] = curCost + map[nextY][nextX];
-                    pq.add(new Status(nextX, nextY, minCost[nextY][nextX]));
+        String[] input = br.readLine().split(" ");
+        int m = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
+        int[][] maze = new int[n][m];
+        int[][] cost = new int[n][m];
+        for (int i=0 ; i<n ; i++) {
+            String row = br.readLine();
+            for (int j=0 ; j<m ; j++) {
+                if (row.charAt(j) == '1') {
+                    maze[i][j] = 1;
                 }
+                cost[i][j] = n + m;
             }
         }
+
+        PriorityQueue<Status> pq = new PriorityQueue<>(Comparator.comparingInt(s -> s.count));
+        pq.add(new Status(0, 0, 0));
+        cost[0][0] = 0;
+        while (!pq.isEmpty()) {
+            Status cur = pq.poll();
+            for (int i=0 ; i<4 ; i++) {
+                int newX = cur.x + xi[i];
+                int newY = cur.y + yi[i];
+                if (!isValid(newX, newY, m, n) || cost[newY][newX] <= cost[cur.y][cur.x] + maze[newY][newX]) continue;
+                cost[newY][newX] = cost[cur.y][cur.x] + maze[newY][newX];
+                pq.add(new Status(newX, newY, cost[newY][newX]));
+            }
+        }
+
+        System.out.println(cost[n - 1][m - 1]);
+        br.close();
     }
 
     private static boolean isValid(int x, int y, int m, int n) {
-        return x>0 && y>0 && x<=m && y<=n;
+        return x>=0 && y>=0 && x<m && y<n;
     }
+
 }
