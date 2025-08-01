66ff40c1d037e11dea8a2d01f74207c2aec6a0e2
juyoungha <htj3460@gmail.com>
2025-08-01 15:14:20 +0900

    Refactor[BOJ - Q.1238] : 솔루션 코드 가독성 개선, 실행시간 단축(1276ms -> 584ms)

--- Dijkstra/BOJ_1238.java
+++ Dijkstra/BOJ_1238.java
@@ -6,76 +6,77 @@ import java.io.*;
 
 public class Main {
 
-    static class Info implements Comparable<Info> {
-        private final int dest;
-        private final int time;
+    static class Route {
+        final int dest;
+        final int cost;
 
-        public Info(int dest, int time) {
+        public Route(int dest, int cost) {
             this.dest = dest;
-            this.time = time;
-        }
-
-        public int getDest() { return this.dest; }
-        public int getTime() { return this.time; }
-
-        @Override
-        public int compareTo(Info o) {
-            return this.time - o.getTime();
+            this.cost = cost;
         }
     }
+
+    static int n, m, x;
+    static List<Route>[] al;
+    static final int INF = 1_000_001;
 
     public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
+
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        int[][] minTimes = new int[n + 1][n + 1];
-        List<Info>[] al = new List[n + 1];
-        for(int i=1 ; i<=n ; i++) {
-            Arrays.fill(minTimes[i], Integer.MAX_VALUE);
-            al[i] = new LinkedList<>();
+        String[] row = br.readLine().split(" ");
+        n = Integer.parseInt(row[0]);
+        m = Integer.parseInt(row[1]);
+        x = Integer.parseInt(row[2]);
+        al = new ArrayList[n + 1];
+        for (int i=1 ; i<=n ; i++) {
+            al[i] = new ArrayList<>();
         }
-
-        for(int i=0 ; i<m ; i++) {
-            st = new StringTokenizer(br.readLine());
-            int src = Integer.parseInt(st.nextToken());
-            int dest = Integer.parseInt(st.nextToken());
-            int time = Integer.parseInt(st.nextToken());
-            al[src].add(new Info(dest, time));
+        for (int i=0 ; i<m ; i++) {
+            row = br.readLine().split(" ");
+            int dept = Integer.parseInt(row[0]);
+            int dest = Integer.parseInt(row[1]);
+            int time = Integer.parseInt(row[2]);
+            al[dept].add(new Route(dest, time));
         }
-
-        for(int i=1 ; i<=n ; i++) {
-            dijkstra(al, i, minTimes);
-        }
-
-        int maxTime = 0;
-        for(int i=1 ; i<=n ; i++) {
-            int time = 0;
-            time += minTimes[i][x];
-            time += minTimes[x][i];
-            if(maxTime < time) {
-                maxTime = time;
-            }
-        }
-
-        System.out.println(maxTime);
-
         br.close();
-        System.exit(0);
     }
 
-    private static void dijkstra(List<Info>[] al, int src, int[][] minTimes) {
-        PriorityQueue<Info> pq = new PriorityQueue<>();
-        pq.add(new Info(src, 0));
-        while(!pq.isEmpty()) {
-            Info curInfo = pq.poll();
-            if(curInfo.getTime() >= minTimes[src][curInfo.getDest()]) continue;
-            minTimes[src][curInfo.getDest()] = curInfo.getTime();
-            for(Info info : al[curInfo.getDest()]) {
-                pq.add(new Info(info.getDest(), minTimes[src][curInfo.getDest()] + info.getTime()));
+    private static int solve() {
+        int[] costForA = new int[n + 1];
+        for (int i=1 ; i<=n ; i++) {
+            if (i == x) continue;
+            int[] cost = dijkstra(i);
+            costForA[i] = cost[x];
+        }
+        int[] costForR = dijkstra(x);
+        int maxCost = 0;
+        for (int i=1 ; i<=n ; i++) {
+            maxCost = Math.max(maxCost, costForA[i] + costForR[i]);
+        }
+        return maxCost;
+    }
+
+    private static int[] dijkstra(int s) {
+        int[] cost = new int[n + 1];
+        Arrays.fill(cost, INF);
+        PriorityQueue<Route> pq = new PriorityQueue<>(Comparator.comparingInt(r -> r.cost));
+        pq.add(new Route(s, 0));
+        cost[s] = 0;
+        while (!pq.isEmpty()) {
+            Route cur = pq.poll();
+            if (cost[cur.dest] < cur.cost) continue;
+            for (Route next : al[cur.dest]) {
+                if (cost[next.dest] > cost[cur.dest] + next.cost) {
+                    cost[next.dest] = cost[cur.dest] + next.cost;
+                    pq.add(new Route(next.dest, cost[next.dest]));
+                }
             }
         }
+        return cost;
     }
+
 }
