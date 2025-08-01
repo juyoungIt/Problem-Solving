07a53de50da7fd1c1382282b002808c9ea434711
juyoungha <htj3460@gmail.com>
2025-08-01 14:36:48 +0900

    Refactor[BOJ - Q.1504] : 솔루션 코드 가독성 개선, 실행시간 단축(1464ms -> 636ms)

--- Dijkstra/BOJ_1504.java
+++ Dijkstra/BOJ_1504.java
@@ -6,91 +6,76 @@ import java.io.*;
 
 public class Main {
 
-    static class Vertex implements Comparable<Vertex> {
-        private final int target;
-        private final int dist;
+    static class Route {
+        final int dept;
+        final int cost;
 
-        public Vertex(int target, int dist) {
-            this.target = target;
-            this.dist = dist;
+        public Route(int dept, int cost) {
+            this.dept = dept;
+            this.cost = cost;
         }
-
-        public int getTarget() { return this.target; }
-        public int getDist() { return this.dist; }
-
-        @Override
-        public int compareTo(Vertex v) {
-            return this.dist - v.getDist();
-        }
-
     }
+
+    static int n, e;
+    static List<Route>[] al;
+    static int v1, v2;
+    static final int INF = 200_000_001;
 
     public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(solve());
+    }
+
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        List<Vertex>[] al = new List[n + 1];
-        for(int i=1 ; i<=n ; i++) {
-            al[i] = new LinkedList<>();
+        String[] row = br.readLine().split(" ");
+        n = Integer.parseInt(row[0]);
+        e = Integer.parseInt(row[1]);
+        al = new ArrayList[n + 1];
+        for (int i=1 ; i<=n ; i++) {
+            al[i] = new ArrayList<>();
         }
-        for(int i=0 ; i<e ; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            al[a].add(new Vertex(b, c));
-            al[b].add(new Vertex(a, c));
+        for (int i=0 ; i<e ; i++) {
+            row = br.readLine().split(" ");
+            int a = Integer.parseInt(row[0]);
+            int b = Integer.parseInt(row[1]);
+            int c = Integer.parseInt(row[2]);
+            al[a].add(new Route(b, c));
+            al[b].add(new Route(a, c));
         }
-        st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
-
-        int[] partA = dijkstra(al, 1);
-        int[] partB = dijkstra(al, v1);
-        int[] partC = dijkstra(al, n);
-
-        int candidate1 = isValid(partA[v1], partB[v2], partC[v2]);
-        int candidate2 = isValid(partA[v2], partB[v2], partC[v1]);
-        if(candidate1 > 0 && candidate2 > 0) {
-            System.out.println(Math.min(candidate1, candidate2));
-        } else if(candidate1 > 0) {
-            System.out.println(candidate1);
-        } else if(candidate2 > 0) {
-            System.out.println(candidate2);
-        } else {
-            System.out.println(-1);
-        }
-
+        row = br.readLine().split(" ");
+        v1 = Integer.parseInt(row[0]);
+        v2 = Integer.parseInt(row[1]);
         br.close();
-        System.exit(0);
     }
 
-    private static int[] dijkstra(List<Vertex>[] al, int src) {
-        int[] minDist = new int[al.length];
-        Arrays.fill(minDist, Integer.MAX_VALUE);
-        PriorityQueue<Vertex> pq = new PriorityQueue<>();
-        pq.add(new Vertex(src, 0));
-        while(!pq.isEmpty()) {
-            Vertex v = pq.poll();
-            if(minDist[v.getTarget()] <= v.getDist()) {
-                continue;
-            }
-            minDist[v.getTarget()] = v.getDist();
-            for(Vertex cv : al[v.getTarget()]) {
-                pq.add(new Vertex(cv.getTarget(), v.getDist() + cv.getDist()));
-            }
-        }
-        return minDist;
+    private static int solve() {
+        int[] costFrom1 = dijkstra(1);
+        int[] costFromV1 = dijkstra(v1);
+        int[] costFromN = dijkstra(n);
+        int costA = costFrom1[v1] + costFromV1[v2] + costFromN[v2];
+        int costB = costFrom1[v2] + costFromV1[v2] + costFromN[v1];
+        int optCost = Math.min(costA, costB);
+        return optCost < INF ? optCost : -1;
     }
 
-    private static int isValid(int minDistA, int minDistB, int minDistC) {
-        if(minDistA != Integer.MAX_VALUE
-                && minDistB != Integer.MAX_VALUE
-                && minDistC != Integer.MAX_VALUE) {
-            return minDistA + minDistB + minDistC;
+    private static int[] dijkstra(int s) {
+        int[] cost = new int[n + 1];
+        Arrays.fill(cost, INF);
+        PriorityQueue<Route> pq = new PriorityQueue<>(Comparator.comparingInt(r -> r.cost));
+        pq.add(new Route(s, 0));
+        cost[s] = 0;
+        while (!pq.isEmpty()) {
+            Route cur = pq.poll();
+            if (cost[cur.dept] < cur.cost) continue;
+            for (Route next : al[cur.dept]) {
+                if (cost[next.dept] > cost[cur.dept] + next.cost) {
+                    cost[next.dept] = cost[cur.dept] + next.cost;
+                    pq.add(new Route(next.dept, cost[next.dept]));
+                }
+            }
         }
-        return -1;
+        return cost;
     }
+    
 }
