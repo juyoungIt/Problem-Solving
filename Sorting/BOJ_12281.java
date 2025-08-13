9bc2bf2191ad582cb816a9de0e67338e326c7f07
juyoungha <htj3460@gmail.com>
2025-08-13 14:44:22 +0900

    Refactor[BOJ - Q.12281] : 솔루션 코드 가독성 개선

--- Sorting/BOJ_12281.java
+++ Sorting/BOJ_12281.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 public class Main {
 
-    private static final String OUTPUT_FORMAT = "Case #%d: %s\n";
+    static final String FORMAT = "Case #%d: %s";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,38 +15,22 @@ public class Main {
         int t = Integer.parseInt(br.readLine());
         for (int i=1 ; i<=t ; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] arr = new int[n];
-            List<Integer> alexBooks = new ArrayList<>();
-            List<Integer> bobBooks = new ArrayList<>();
-            List<Integer> alexIndexes = new ArrayList<>();
-            List<Integer> bobIndexes = new ArrayList<>();
+            int[] labels = new int[n];
+            PriorityQueue<Integer> pqA = new PriorityQueue<>();
+            PriorityQueue<Integer> pqB = new PriorityQueue<>(Collections.reverseOrder());
             String[] row = br.readLine().split(" ");
             for (int j=0 ; j<n ; j++) {
-                arr[j] = Integer.parseInt(row[j]);
-                if (Math.abs(arr[j] % 2) == 1) {
-                    alexBooks.add(arr[j]);
-                    alexIndexes.add(j);
-                } else {
-                    bobBooks.add(arr[j]);
-                    bobIndexes.add(j);
-                }
+                int value = Integer.parseInt(row[j]);
+                labels[j] = value % 2;
+                if (labels[j] == 0) pqB.add(value);
+                else pqA.add(value);
             }
-            Collections.sort(alexBooks);
-            bobBooks.sort(Collections.reverseOrder());
-
-            for (int j=0 ; j<alexBooks.size() ; j++) {
-                arr[alexIndexes.get(j)] = alexBooks.get(j);
-            }
-
-            for (int j=0 ; j<bobBooks.size() ; j++) {
-                arr[bobIndexes.get(j)] = bobBooks.get(j);
-            }
-
             StringBuilder buffer = new StringBuilder();
-            for (int element : arr) {
-                buffer.append(element).append(" ");
+            for (int j=0 ; j<n ; j++) {
+                if (labels[j] == 0) buffer.append(pqB.poll()).append(" ");
+                else buffer.append(pqA.poll()).append(" ");
             }
-            sb.append(String.format(OUTPUT_FORMAT, i, buffer));
+            sb.append(String.format(FORMAT, i, buffer)).append("\n");
         }
 
         System.out.println(sb);
