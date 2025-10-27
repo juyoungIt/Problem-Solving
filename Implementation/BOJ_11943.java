ae0e436ce95dc53020bae66509ee9ae9fdbdd12b
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2025-10-28 08:32:02 +0900

    Solution[BOJ - Q.11943]

---
+++ Implementation/BOJ_11943.java
@@ -0,0 +1,19 @@
+// BOJ - 11943
+// Problem Sheet - https://www.acmicpc.net/problem/11943
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        System.out.println(Math.min(a + d, b + c));
+        br.close();
+    }
+}
