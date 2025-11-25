8b47b5b52da9c4434071739d0d0e935684dd9080
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2025-11-26 08:35:10 +0900

    Solution[BOJ - Q.34761]

---
+++ Implementation/BOJ_34761.java
@@ -0,0 +1,39 @@
+// BOJ - 34761
+// Problem Sheet - https://www.acmicpc.net/problem/34761
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> elementSet = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i=0 ; i<n ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            elementSet.add(arr[i]);
+        }
+        st = new StringTokenizer(br.readLine());
+        br.close();
+        boolean isAvailable = true;
+        // 1. 이미 만들어진 부분 -> 순서가 완벽히 동일해야 함
+        for (int i=0 ; i<n ; i++) {
+            if (arr[i] != Integer.parseInt(st.nextToken())) {
+                isAvailable = false;
+                break;
+            }
+        }
+        // 2. 앞으로 만들어낼 부분 -> 배열A가 가진 원소들만을 가지고 있어야 함
+        if (isAvailable) {
+            for (int i=n ; i<n*2 ; i++) {
+                if (!elementSet.contains(Integer.parseInt(st.nextToken()))) {
+                    isAvailable = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(isAvailable ? "YES" : "NO");
+    }
+}
