985e78984829ded7520ec1d9301970888c4389ba
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2025-12-25 00:02:12 +0900

    Solution[BOJ - Q.5338]

---
+++ Implementation/BOJ_5338.java
@@ -0,0 +1,15 @@
+// BOJ - 5338
+// Problem Sheet - https://www.acmicpc.net/problem/5338
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
