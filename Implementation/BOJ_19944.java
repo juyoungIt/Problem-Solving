09c8d1dbee9b7386a8acdb19691e55ec07186d69
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2025-12-27 18:07:16 +0900

    Solution[BOJ - Q.19944]

---
+++ Implementation/BOJ_19944.java
@@ -0,0 +1,22 @@
+// BOJ - 19944
+// Problem Sheet - https://www.acmicpc.net/problem/19944
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        if (m <= 2) {
+            System.out.println("NEWBIE!");
+        } else if (m <= n) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TLE!");
+        }
+        br.close();
+    }
+}
