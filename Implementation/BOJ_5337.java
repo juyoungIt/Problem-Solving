a08b486192b411ea006581aeb55a0cbc58594770
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2026-01-08 23:58:56 +0900

    Solution[BOJ - Q.5337]

---
+++ Implementation/BOJ_5337.java
@@ -0,0 +1,10 @@
+// BOJ - 5337
+// Problem Sheet - https://www.acmicpc.net/problem/5337
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
