fd4a59dd4c34fa4f1d7e08c8f7d01e0b377977f9
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2026-01-09 21:21:38 +0900

    Refactor[BOJ - Q.30402]

--- Implementation/BOJ_30402.java
+++ Implementation/BOJ_30402.java
@@ -8,26 +8,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        String result = "";
         for (int i = 0; i < 15; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 15; j++) {
                 char p = st.nextToken().charAt(0);
                 if (p == 'w') {
-                    result = "chunbae";
-                    break;
+                    System.out.println("chunbae");
+                    return;
                 } else if (p == 'b') {
-                    result = "nabi";
-                    break;
+                    System.out.println("nabi");
+                    return;
                 } else if (p == 'g') {
-                    result = "yeongcheol";
-                    break;
+                    System.out.println("yeongcheol");
+                    return;
                 } else {
                     continue;
                 }
             }
         }
-        System.out.println(result);
         br.close();
     }
 }
