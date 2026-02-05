32ec26326a5ffdeb5bd7331eb25a643594a7dac1
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2026-02-05 19:26:32 +0900

    Refactor[BOJ - Q.1010]

--- DP/BOJ_1010.java
+++ DP/BOJ_1010.java
@@ -5,27 +5,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static long[][] comb; // 각 수마다의 factorial 값을 저장
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        comb = new long[30][30]; // for dp
 
-        int tcSize = Integer.parseInt(bf.readLine()); // 테스트 케이스 수
-        for(int i=0 ; i<tcSize ; i++) {
-            st = new StringTokenizer(bf.readLine());
+    static long[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        dp = new long[30][30];
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-            System.out.println(combination(m, n));
+            sb.append(comb(m, n)).append("\n");
         }
-
-        bf.close();
-        System.exit(0);
+        System.out.println(sb);
+        br.close();
     }
 
-    static long combination(int n, int r) {
-        if(n == r || r == 0) return comb[n][r] = 1;
-        if(comb[n][r] > 0) return comb[n][r];
-        else return comb[n][r] = combination(n-1, r-1) + combination(n-1, r);
+    private static long comb(int n, int r) {
+        if (n == r || r == 0) return dp[n][r] = 1;
+        else if (dp[n][r] > 0) return dp[n][r];
+        else return dp[n][r] = comb(n - 1, r - 1) + comb(n - 1, r);
     }
 }
