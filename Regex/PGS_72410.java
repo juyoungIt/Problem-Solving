73e1b653b96798925793c6325ea32f7f508ff03c
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2026-07-03 22:30:13 +0900

    Refactor[PGS - Q.72410]: 불필요한 주석제거

--- Regex/PGS_72410.java
+++ Regex/PGS_72410.java
@@ -5,24 +5,17 @@ import java.util.*;
 
 class Solution {
     public String solution(String new_id) {
-        // 1단계
         new_id = new_id.toLowerCase();
-        // 2단계
         new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
-        // 3단계
         new_id = new_id.replaceAll("\\.{2,}", ".");
-        // 4단계
         new_id = new_id.replaceAll("^\\.|\\.$", "");
-        // 5단계
         if (new_id.isEmpty()) {
             new_id = "a";
         }
-        // 6단계
         if (new_id.length() >= 16) {
             new_id = new_id.substring(0, 15);
             new_id = new_id.replaceAll("\\.$", "");
         }
-        // 7단계
         if (new_id.length() <= 2) {
             StringBuilder sb = new StringBuilder(new_id);
             char lastChar = new_id.charAt(new_id.length() - 1);
