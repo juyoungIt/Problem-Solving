c2a7e4e5f98bf4dcd695c50eaf1a905bc4a0930c
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2026-07-04 19:35:10 +0900

    Refactor[PGS - Q.67256]: 거리계산 및 좌표관리 방식 최적화

--- Implementation/PGS_67256.java
+++ Implementation/PGS_67256.java
@@ -5,50 +5,32 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers, String hand) {
-        Map<Character, Integer> xIndexMap = new HashMap<>();
-        xIndexMap.put('1', 0); xIndexMap.put('4', 0); xIndexMap.put('7', 0); xIndexMap.put('*', 0);
-        xIndexMap.put('2', 1); xIndexMap.put('5', 1); xIndexMap.put('8', 1); xIndexMap.put('0', 1);
-        xIndexMap.put('3', 2); xIndexMap.put('6', 2); xIndexMap.put('9', 2); xIndexMap.put('#', 2);
-        Map<Character, Integer> yIndexMap = new HashMap<>();
-        yIndexMap.put('1', 0); yIndexMap.put('4', 1); yIndexMap.put('7', 2); yIndexMap.put('*', 3);
-        yIndexMap.put('2', 0); yIndexMap.put('5', 1); yIndexMap.put('8', 2); yIndexMap.put('0', 3);
-        yIndexMap.put('3', 0); yIndexMap.put('6', 1); yIndexMap.put('9', 2); yIndexMap.put('#', 3);
-        int curLeftX = xIndexMap.get('*'), curLeftY = yIndexMap.get('*');
-        int curRightX = xIndexMap.get('#'), curRightY = yIndexMap.get('#');
         StringBuilder sb = new StringBuilder();
+        int curLeftIndex = 10, curRightIndex = 12;
         for (int number : numbers) {
+            if (number == 0) number = 11;
             if (number == 1 || number == 4 || number == 7) {
                 sb.append("L");
-                curLeftX = xIndexMap.get((char) (number + '0'));
-                curLeftY = yIndexMap.get((char) (number + '0'));
+                curLeftIndex = number;
             } else if (number == 3 || number == 6 || number == 9) {
                 sb.append("R");
-                curRightX = xIndexMap.get((char) (number + '0'));
-                curRightY = yIndexMap.get((char) (number + '0'));
+                curRightIndex = number;
             } else {
-                int targetX = xIndexMap.get((char) (number + '0'));
-                int targetY = yIndexMap.get((char) (number + '0'));
-                int leftDist = getDist(curLeftX, curLeftY, targetX, targetY);
-                int rightDist = getDist(curRightX, curRightY, targetX, targetY);
-                if (leftDist == rightDist) {
+                int leftDist = getDist(curLeftIndex, number);
+                int rightDist = getDist(curRightIndex, number);
+                if (leftDist < rightDist) {
+                    sb.append("L");
+                    curLeftIndex = number;
+                } else if (leftDist > rightDist) {
+                    sb.append("R");
+                    curRightIndex = number;
+                } else {
                     if (hand.equals("left")) {
                         sb.append("L");
-                        curLeftX = xIndexMap.get((char) (number + '0'));
-                        curLeftY = yIndexMap.get((char) (number + '0'));
+                        curLeftIndex = number;
                     } else {
                         sb.append("R");
-                        curRightX = xIndexMap.get((char) (number + '0'));
-                        curRightY = yIndexMap.get((char) (number + '0'));
-                    }
-                } else {
-                    if (leftDist < rightDist) {
-                        sb.append("L");
-                        curLeftX = xIndexMap.get((char) (number + '0'));
-                        curLeftY = yIndexMap.get((char) (number + '0'));
-                    } else {
-                        sb.append("R");
-                        curRightX = xIndexMap.get((char) (number + '0'));
-                        curRightY = yIndexMap.get((char) (number + '0'));
+                        curRightIndex = number;
                     }
                 }
             }
@@ -56,42 +38,11 @@ class Solution {
         return sb.toString();
     }
     
-    private int getDist(int startX, int startY, int targetX, int targetY) {
-        int[] xi = {-1, 1, 0, 0};
-        int[] yi = {0, 0, -1, 1};
-        boolean[][] visited = new boolean[4][3];
-        Queue<Integer> queueX = new LinkedList<>();
-        Queue<Integer> queueY = new LinkedList<>();
-        Queue<Integer> queueD = new LinkedList<>();
-        queueX.add(startX);
-        queueY.add(startY);
-        queueD.add(0);
-        visited[startY][startX] = true;
-        while (!queueX.isEmpty() && !queueY.isEmpty()) {
-            int curX = queueX.peek();
-            int curY = queueY.peek();
-            int curD = queueD.peek();
-            if (curX == targetX && curY == targetY) {
-                return curD;
-            }
-            for (int i=0; i<4; i++) {
-                int newX = curX + xi[i];
-                int newY = curY + yi[i];
-                if (!isValid(newX, newY) || visited[newY][newX]) continue;
-                queueX.add(newX);
-                queueY.add(newY);
-                queueD.add(curD + 1);
-                visited[newY][newX] = true;
-            }
-            queueX.poll();
-            queueY.poll();
-            queueD.poll();
-        }
-        return -1;
-    }
-    
-    
-    private boolean isValid(int x, int y) {
-        return x>=0 && y>=0 && x<3 && y<4;
+    private int getDist(int curIndex, int targetIndex) {
+        int curX = (curIndex - 1) / 3;
+        int curY = (curIndex - 1) % 3;
+        int targetX = (targetIndex - 1) / 3;
+        int targetY = (targetIndex - 1) % 3;
+        return Math.abs(curX - targetX) + Math.abs(curY - targetY);
     }
 }
