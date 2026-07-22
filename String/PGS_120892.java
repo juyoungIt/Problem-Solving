06a424f43d6e788724519c43fd341335bb1c3ffa
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2026-07-22 23:30:55 +0900

    Solution[PGS - Q.120892]

---
+++ String/PGS_120892.java
@@ -0,0 +1,14 @@
+// PGS - 120892
+// Problem Sheet - https://school.programmers.co.kr/learn/courses/30/lessons/120892
+
+import java.util.*;
+
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=code-1; i<cipher.length(); i+=code) {
+            sb.append(cipher.charAt(i));
+        }
+        return sb.toString();
+    }
+}
