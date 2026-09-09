9d650374c58d38f1d8e729490f944e143c85bbaf
juyoungha <62272939+juyoungIt@users.noreply.github.com>
2026-09-09 22:03:34 +0900

    Solution[PGS - Q.181903]

---
+++ Implementation/PGS_181903.java
@@ -0,0 +1,14 @@
+// PGS - 181903
+// Problem Sheet - https://school.programmers.co.kr/learn/courses/30/lessons/181903
+
+class Solution {
+    public String solution(int q, int r, String code) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<code.length(); i++) {
+            if (i % q == r) {
+                sb.append(code.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
